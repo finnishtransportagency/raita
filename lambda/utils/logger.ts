91a8ce9544bd4bb9699892ddc5d367962f3c31c4
Ari Ahonen <ari.ahonen@nordcloud.com>
2022-09-19 10:34:50 +0300

    refactor: 💡 improve logger.log error handling

--- lambda/utils/logger.ts
+++ lambda/utils/logger.ts
@@ -1,19 +1,27 @@
-import { S3EventRecord } from "aws-lambda/trigger/s3";
+import { S3EventRecord } from 'aws-lambda/trigger/s3';
 
 class Logger {
-  log = (data: any) => {
+  log = (data: unknown) => {
     const message =
-      (typeof data === "string" && data) ||
+      (typeof data === 'string' && data) ||
       (data instanceof Error && data.message) ||
       JSON.stringify(data);
-    console.log(message);
+    try {
+      console.log(message);
+    } catch (error) {
+      console.log(
+        `Logger failed to log a given message: ${
+          error instanceof Error && error.message
+        }`,
+      );
+    }
   };
 
   logS3EventRecord = (eventRecord: S3EventRecord) => {
     this.log(`File arn: ${eventRecord.s3.bucket.arn}`);
     this.log(`File object key: ${eventRecord.s3.object.key}`);
     this.log(`File bucket: ${eventRecord.s3.bucket.name}`);
-    this.log(`File path: ${eventRecord.s3.object.key.split("/")}`);
+    this.log(`File path: ${eventRecord.s3.object.key.split('/')}`);
   };
 }
 
