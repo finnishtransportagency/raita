4d0a084eb5fa2d6b23a794a67d4264a35fb89dd8
Ari Ahonen <ari.ahonen@nordcloud.com>
2022-09-20 10:26:16 +0300

    fix: set region to specified string (temporarily)

--- lambda/config/index.ts
+++ lambda/config/index.ts
@@ -12,7 +12,8 @@ const ConfigSchema = z.object({
 });
 
 // CDK DEFAULTS come from the AWS profile that is used to run the commands
-// TODO: This may not be the brightest implementation at the moment.
+// TODO: This IS not be the brightest implementation at the moment.
+// This needs to be fixed (at least) by separating SYNTH time and RUNTIME variables, see
 // TODO: Configuration file now hardcoded
 const getConfig = () => {
   const config = {
@@ -20,7 +21,7 @@ const getConfig = () => {
     configurationBucket: process.env.CONFIGURATION_BUCKET || 'UNDEFINED',
     configurationFile: 'extractionSpec.json',
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION, // OR 'eu-west-1' OR process.env.AWS_REGION
+    region: 'eu-west-1', // process.env.CDK_DEFAULT_REGION, // OR 'eu-west-1' OR process.env.AWS_REGION
     openSearchMetadataIndex: 'metadata-index',
   };
   return ConfigSchema.parse(config);
