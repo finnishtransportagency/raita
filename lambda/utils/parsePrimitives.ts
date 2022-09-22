864cdd66ca013c026a4b302cd5a69fbb5f572a15
Ari Ahonen <ari.ahonen@nordcloud.com>
2022-09-22 06:45:35 +0300

    feat(parser): parse date to ISOString

--- lambda/utils/parsePrimitives.ts
+++ lambda/utils/parsePrimitives.ts
@@ -1,14 +1,14 @@
-import { TargetBaseProps } from 'aws-cdk-lib/aws-events-targets';
+import parse from 'date-fns/parse';
 
 export const parsePrimitive = (
   key: string,
   data: string,
   target: 'integer' | 'float' | 'date',
 ) => {
-  const parsers: Record<typeof target, (x: string) => number> = {
+  const parsers: Record<typeof target, (x: string) => number | string> = {
     integer: x => parseInt(x),
     float: x => parseFloat(x),
-    date: x => new Date(x).valueOf(),
+    date: x => parse(x, 'M/d/y h:m:s a', new Date()).toISOString(),
   };
   try {
     return { key, value: parsers[target](data) };
