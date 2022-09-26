bd8f337c1d09a2d778638b3d7936db37ec12f147
Stefan Rimaila <stefan@rimaila.fi>
2022-09-26 08:18:44 +0300

    chore(frontend): clean up root

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Datepicker, Dropdown } from './components';
+import { items } from './dummy';
+import { toItem } from './shared/util';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="cols pad">
+        <div>
+          <form className="space-y">
+            <fieldset>
+              <legend>Hakuehdot</legend>
+
+              <Dropdown caption={'Ehto 1'} items={[]} />
+              <Dropdown caption={'Ehto 2'} items={[]} />
+            </fieldset>
+
+            <fieldset>
+              <legend>Aikaväli</legend>
+
+              <div className="cols">
+                <Datepicker caption={'Alku'} />
+                <Datepicker caption={'Loppu'} />
+              </div>
+            </fieldset>
+
+            <fieldset>
+              <legend>Raporttityypit</legend>
+
+              <Dropdown
+                caption={'Tyyppi'}
+                multiple={true}
+                items={[toItem('one', 'One'), toItem('two', 'Two')]}
+              />
+            </fieldset>
+
+            <button>Hae raportit</button>
+          </form>
+        </div>
+
+        <div className="space-y">
+          <section>
+            <header>Tulokset</header>
+
+            <ul>
+              {items.map((it, i) => (
+                <li key={it.key}>{it.key}</li>
+              ))}
+            </ul>
+
+            <div className="space-y">
+              <ul className="pager">
+                <li className="pager__item">Prev</li>
+                <li className="pager__item">1</li>
+                <li className="pager__item">2</li>
+                <li className="pager__item">Next</li>
+              </ul>
+
+              <button>Lataa kaikki</button>
+            </div>
+
+            <footer>
+              <Dropdown
+                caption={'Tuloksia/sivu'}
+                items={[
+                  toItem('one', '5'),
+                  toItem('two', '10'),
+                  toItem('three', '15'),
+                ]}
+              />
+            </footer>
+          </section>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import 'normalize.css';
+import './index.css';
+
+import App from './App';
+import { client } from './core/query';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={client}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  </StrictMode>,
+);
