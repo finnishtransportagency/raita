a90230ebb30f559c9b8daf4195a40f22c73426b5
Stefan Rimaila <stefan@rimaila.fi>
2022-09-30 06:54:08 +0300

    chore(frontend): normalize stylesheets

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 
+import 'normalize.css';
 import '../styles/globals.css';
 
 const client = new QueryClient();
