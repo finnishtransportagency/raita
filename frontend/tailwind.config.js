4d539205692ae6659b98506acb8b0734ea433b3d
Stefan Rimaila <stefan@rimaila.fi>
2022-10-18 23:12:20 +0300

    chore(frontend): update tailwind configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0064AF',
+        'main-black-100': '#000',
+        'main-gray-75': '#333',
+        'main-gray-50': '#999',
+      },
+      fontFamily: {
+        sans: ['"Exo 2"'],
+        mono: ['"Cascadia Code"'],
+      },
+    },
   },
   plugins: [],
 };
