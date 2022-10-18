61e9cc2a13d21a22cdfca783fd18aa9b67056b38
Stefan Rimaila <stefan@rimaila.fi>
2022-10-18 22:50:04 +0300

    chore(frontend): clean up redundant components

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -1,5 +1,2 @@
 export { default as Dropdown } from './dropdown';
-export { default as Input } from './input';
-export { default as Pager } from './pager';
 export { default as Button } from './button';
-export { default as Layout } from './layout';
