1fc23846eb169dc2c9b86e265db513920d823344
Stefan Rimaila <stefan@rimaila.fi>
2022-10-19 22:24:12 +0300

    feat(frontend): initial pager impl

---
+++ frontend/pages/reports/components/pager.tsx
@@ -0,0 +1,46 @@
+import { repeat } from 'rambda';
+import { clsx } from 'clsx';
+
+import css from './pager.module.css';
+
+export function Pager(props: Props) {
+  const { count, size, page } = props;
+  const currentPage = page + 1;
+
+  const pages = Math.ceil(count / size);
+  const pageList = repeat(0, pages).map((_, ix) => ({ page: ix + 1 }));
+
+  return (
+    <>
+      <nav className={clsx(css.root)}>
+        <ul className={clsx('flex items-center space-x-2', css.pager)}>
+          {pageList.map((n, ix) => (
+            <li
+              key={ix}
+              className={clsx(
+                css.page,
+                n.page === currentPage && css.pageCurrent,
+              )}
+            >
+              {n.page}
+            </li>
+          ))}
+        </ul>
+        <ul></ul>
+      </nav>
+    </>
+  );
+}
+
+export default Pager;
+
+//
+
+export type Props = {
+  count: number;
+  /**
+   * Page size
+   */
+  size: number;
+  page: number;
+};
