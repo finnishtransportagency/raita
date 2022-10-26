664aed550d4535549b78a5eb8d3532de1a840710
Ari Ahonen <ari.ahonen@nordcloud.com>
2022-10-26 17:04:29 +0300

    update: remove s3 deploy test setup

--- lib/raita-cloudfront.ts
+++ lib/raita-cloudfront.ts
@@ -27,16 +27,6 @@ export class CloudfrontStack extends Stack {
       cloudfrontDomainName,
     } = props;
 
-    // const frontendBucket = new s3.Bucket(this, 'frontend', {
-    //   bucketName: `s3-${raitaStackId}-frontend`,
-    //   publicReadAccess: false,
-    //   accessControl: s3.BucketAccessControl.PRIVATE,
-    //   blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-    //   removalPolicy: getRemovalPolicy(raitaEnv),
-    //   objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
-    //   encryption: s3.BucketEncryption.S3_MANAGED,
-    // });
-
     const frontendBucket = createRaitaBucket({
       scope: this,
       name: 'frontend',
@@ -44,52 +34,53 @@ export class CloudfrontStack extends Stack {
       raitaStackIdentifier,
     });
 
+    // Deploy pipeline front end build products to S3 bucket
     const buildDir = '../frontend/out';
     new BucketDeployment(this, 'FrontendDeployment', {
       sources: [Source.asset(path.join(__dirname, buildDir))],
       destinationBucket: frontendBucket,
     });
 
-    // const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
-    //   this,
-    //   'CloudFrontOriginAccessIdentity',
-    // );
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
+      this,
+      'CloudFrontOriginAccessIdentity',
+    );
 
-    // frontendBucket.addToResourcePolicy(
-    //   new iam.PolicyStatement({
-    //     actions: ['s3:GetObject'],
-    //     resources: [frontendBucket.arnForObjects('*')],
-    //     principals: [
-    //       new iam.CanonicalUserPrincipal(
-    //         cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId,
-    //       ),
-    //     ],
-    //   }),
-    // );
+    frontendBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [frontendBucket.arnForObjects('*')],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId,
+          ),
+        ],
+      }),
+    );
 
-    // const certificate = acm.Certificate.fromCertificateArn(
-    //   this,
-    //   `certificate-${raitaStackIdentifier}`,
-    //   cloudfrontCertificateArn,
-    // );
+    const certificate = acm.Certificate.fromCertificateArn(
+      this,
+      `certificate-${raitaStackIdentifier}`,
+      cloudfrontCertificateArn,
+    );
 
-    // const cloudfrontDistribution = new cloudfront.Distribution(
-    //   this,
-    //   `cloudfront`,
-    //   {
-    //     domainNames: [cloudfrontDomainName],
-    //     certificate,
-    //     defaultRootObject: 'index.html',
-    //     comment: `cloudfront for ${raitaStackIdentifier}`,
-    //     priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
-    //     defaultBehavior: {
-    //       origin: new origins.S3Origin(frontendBucket, {
-    //         originAccessIdentity: cloudfrontOAI,
-    //       }),
-    //       viewerProtocolPolicy:
-    //         cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-    //     },
-    //   },
-    // );
+    const cloudfrontDistribution = new cloudfront.Distribution(
+      this,
+      `cloudfront`,
+      {
+        domainNames: [cloudfrontDomainName],
+        certificate,
+        defaultRootObject: 'index.html',
+        comment: `cloudfront for ${raitaStackIdentifier}`,
+        priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+        defaultBehavior: {
+          origin: new origins.S3Origin(frontendBucket, {
+            originAccessIdentity: cloudfrontOAI,
+          }),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+      },
+    );
   }
 }
