904b01af4bbbf39a73ac550e59b661aa304daf10
Ari Ahonen <ari.ahonen@nordcloud.com>
2022-10-27 10:17:02 +0300

    refactor: frontend nested stack

--- lib/raita-cloudfront.ts
+++ lib/raita-cloudfront.ts
@@ -5,41 +5,26 @@ import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Construct } from 'constructs';
 import { RaitaEnvironment } from './config';
-import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
-import * as path from 'path';
-import { createRaitaBucket } from './raitaResourceCreators';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
-interface FrontendInfraStackProps extends StackProps {
+interface CloudfrontStackProps extends StackProps {
   readonly raitaStackIdentifier: string;
   readonly raitaEnv: RaitaEnvironment;
   readonly cloudfrontCertificateArn: string;
   readonly cloudfrontDomainName: string;
+  readonly frontendBucket: Bucket;
 }
 
 // Based on: https://idanlupinsky.com/blog/static-site-deployment-using-aws-cloudfront-and-the-cdk/
 export class CloudfrontStack extends Stack {
-  constructor(scope: Construct, id: string, props: FrontendInfraStackProps) {
+  constructor(scope: Construct, id: string, props: CloudfrontStackProps) {
     super(scope, id, props);
     const {
-      raitaEnv,
       raitaStackIdentifier,
       cloudfrontCertificateArn,
       cloudfrontDomainName,
+      frontendBucket,
     } = props;
-
-    const frontendBucket = createRaitaBucket({
-      scope: this,
-      name: 'frontend',
-      raitaEnv,
-      raitaStackIdentifier,
-    });
-
-    // Deploy pipeline front end build products to S3 bucket
-    const buildDir = '../frontend/out';
-    new BucketDeployment(this, 'FrontendDeployment', {
-      sources: [Source.asset(path.join(__dirname, buildDir))],
-      destinationBucket: frontendBucket,
-    });
 
     const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
       this,
