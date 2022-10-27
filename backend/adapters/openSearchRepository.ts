0efaaa96dba0ad1422de7205d6479537404ab2f2
Ari Ahonen <ari.ahonen@nordcloud.com>
2022-10-27 19:12:44 +0300

    debug: add logging

--- backend/adapters/openSearchRepository.ts
+++ backend/adapters/openSearchRepository.ts
@@ -1,6 +1,5 @@
 import { FileMetadataEntry } from '../types';
 import { IMetadataStorageInterface } from '../types/portDataStorage';
-import { logger } from '../utils/logger';
 import { RaitaOpenSearchClient } from '../clients/openSearchClient';
 
 /**
@@ -24,6 +23,7 @@ export class OpenSearchRepository implements IMetadataStorageInterface {
   saveFileMetadata = async (data: Array<FileMetadataEntry>) => {
     const client = await this.#openSearchClient.getClient();
     // Add entries to the index.
+    console.log('Persisting data to store');
     const additions = data.map(async entry => {
       const addDocresponse = client.index({
         index: this.#dataIndex,
@@ -31,18 +31,24 @@ export class OpenSearchRepository implements IMetadataStorageInterface {
       });
       return addDocresponse;
     });
-    await Promise.all(additions).catch(err => {
-      throw err;
-    });
+    await Promise.all(additions)
+      .then(() => {
+        console.log('Data persisted to open search.');
+      })
+      .catch(err => {
+        throw err;
+      });
   };
 
   // TODO: Provide best possible types
   queryOpenSearchMetadata = async (query: any) => {
     const client = await this.#openSearchClient.getClient();
+    console.log('Starting os query.');
     const response = await client.search({
       index: this.#dataIndex,
       body: query,
     });
+    console.log('Data queried from open search.');
     return response;
   };
 }
