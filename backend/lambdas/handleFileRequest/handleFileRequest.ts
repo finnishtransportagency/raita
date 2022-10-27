5cd375b538748acc00a0186cc43d3332415a251b
Ari Ahonen <ari.ahonen@nordcloud.com>
2022-10-27 16:20:07 +0300

    debug: api lambda access debugging

--- backend/lambdas/handleFileRequest/handleFileRequest.ts
+++ backend/lambdas/handleFileRequest/handleFileRequest.ts
@@ -27,13 +27,13 @@ export async function handleFileRequest(
   const s3 = new S3();
   try {
     const { dataBucket } = getLambdaConfigOrFail();
+    console.log(event.body);
     const requestBody = body && JSON.parse(body);
     if (!requestBody?.key) {
       throw new Error('Key not specified');
     }
     const { key } = requestBody;
 
-    console.log(event.body);
     console.log(requestBody);
 
     // Check if file exists
