bd0e7f2e645de5e3b4771faf393b00457b9028de
Stefan Rimaila <stefan@rimaila.fi>
2022-11-09 09:39:20 +0200

    chore(frontend): update local API routes to reflect real API

--- frontend/pages/api/files.ts
+++ frontend/pages/api/files.ts
@@ -1,5 +1,7 @@
 import { SearchResponse } from '@opensearch-project/opensearch/api/types';
 import { NextApiHandler } from 'next';
+import * as R from 'rambda';
+
 import { client } from 'shared/rest';
 import { IDocument } from 'shared/types';
 
@@ -10,11 +12,9 @@ const handler: NextApiHandler = (req, res) => {
     .get<SearchResponse<IDocument>>(`/${cfg.openSearch.indexName}/_search`, {
       data: req.body,
     })
-    .then(r => {
-      console.log({ r });
-      return r;
-    })
-    .then(r => res.status(200).json(r.data))
+    .then(r => ({ result: { body: r.data } }))
+    .then(R.tap(console.log))
+    .then(r => res.status(200).json(r))
     .catch(err => {
       console.log({ err });
       res.status(500).json({ err });
