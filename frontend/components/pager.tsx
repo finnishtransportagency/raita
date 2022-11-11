7f66542ff0afcfcc9a43cec5807978f3d2c5ec77
Stefan Rimaila <stefan@rimaila.fi>
2022-11-11 11:39:19 +0200

    fix(frontend): add missing translations for pager buttons

--- frontend/components/pager.tsx
+++ frontend/components/pager.tsx
@@ -1,35 +1,57 @@
-import { repeat } from 'rambda';
+import * as R from 'rambda';
 import { clsx } from 'clsx';
 
 import css from './pager.module.css';
+import { useTranslation } from 'next-i18next';
+import { useMemo } from 'react';
 
 export function Pager(props: Props) {
   const { count, size, page, onGotoPage } = props;
-  const currentPage = page + 1;
+  const { t } = useTranslation(['common']);
 
-  const pages = Math.ceil(count / size);
-  const pageList = repeat(0, pages || 1).map((_, ix) => ({ page: ix + 1 }));
+  const currentPage = page + 1;
+  const pages = Math.ceil(count / size) || 1;
+
+  const pageList = useMemo(
+    () => R.repeat(0, pages).map((_, ix) => ({ page: ix + 1 })),
+    [pages],
+  );
 
   return (
     <>
-      <nav className={clsx(css.root)}>
+      <nav
+        className={clsx(css.root)}
+        role="navigation"
+        aria-label={t('common:pager_a11y_label')}
+      >
         <ul className={clsx('flex items-center space-x-2', css.pager)}>
-          {pageList.map((n, ix) => (
-            <li
-              key={ix}
-              className={clsx(
-                css.page,
-                n.page === currentPage && css.pageCurrent,
-              )}
-            >
-              <button
-                className={clsx(css.pageButton)}
-                onClick={() => onGotoPage && onGotoPage(n.page)}
+          {pageList.map(n => {
+            return (
+              <li
+                key={`page-${n.page}`}
+                className={clsx(
+                  css.page,
+                  n.page === currentPage && css.pageCurrent,
+                )}
               >
-                {n.page}
-              </button>
-            </li>
-          ))}
+                <button
+                  {...{
+                    className: css.pageButton,
+                    onClick: () => onGotoPage && onGotoPage(n.page),
+                    'aria-label': t('common:pager_a11y_goto_page', {
+                      page: n.page,
+                    }),
+
+                    // Use `undefined` as value to make this attribute not appear
+                    // for the element instead of `false`
+                    'aria-current': n.page === currentPage ? true : undefined,
+                  }}
+                >
+                  {n.page}
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </>
