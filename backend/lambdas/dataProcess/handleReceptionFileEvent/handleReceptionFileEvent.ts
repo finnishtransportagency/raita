1e39b51dc33b51899ff1cf9657b6bca44ba5f580
Ari Ahonen <ari.ahonen@nordcloud.com>
2022-12-04 13:44:58 +0200

    fix: getkeydata import

--- backend/lambdas/dataProcess/handleReceptionFileEvent/handleReceptionFileEvent.ts
+++ backend/lambdas/dataProcess/handleReceptionFileEvent/handleReceptionFileEvent.ts
@@ -4,7 +4,7 @@ import { logger } from '../../../utils/logger';
 import { getGetEnvWithPreassignedContext } from '../../../../utils';
 import {
   decodeS3EventPropertyString,
-  getKeyConstituents,
+  getKeyData,
   isZipPath,
   RaitaLambdaError,
 } from '../../utils';
@@ -28,7 +28,7 @@ export async function handleReceptionFileEvent(event: S3Event): Promise<void> {
       const config = getLambdaConfigOrFail();
       const bucket = eventRecord.s3.bucket;
       const key = decodeS3EventPropertyString(eventRecord.s3.object.key);
-      const { path, fileSuffix } = getKeyConstituents(key);
+      const { path, fileSuffix } = getKeyData(key);
       if (!isZipPath(path)) {
         throw new RaitaLambdaError(`Unexpected file path ${path}`, 400);
       }
