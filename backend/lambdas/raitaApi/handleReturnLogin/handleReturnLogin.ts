fc85527226aa35d7d49fa3372ef9e1c583fcd61f
Niko Mehtonen <niko.mehtonen@nordcloud.com>
2022-12-07 09:19:48 +0200

    feat: Refactor _event to event based on feedback

--- backend/lambdas/raitaApi/handleReturnLogin/handleReturnLogin.ts
+++ backend/lambdas/raitaApi/handleReturnLogin/handleReturnLogin.ts
@@ -4,14 +4,14 @@ import { getUser } from '../../../utils/userService';
 
 const CLOUDFRONT_DOMAIN_NAME = process.env.CLOUDFRONT_DOMAIN_NAME;
 
-export async function handleRequest(_event: ALBEvent, _context: Context) {
-  const user = await getUser(_event);
+export async function handleRequest(event: ALBEvent, _context: Context) {
+  const user = await getUser(event);
   log.info(user, 'Returning user back to frontpage.');
 
   const expires = new Date(Date.now() + 120 * 1000).toUTCString(); // In two minutes
   const setCookieAttributes = `; Domain=${CLOUDFRONT_DOMAIN_NAME}; Path=/; Secure; SameSite=Lax; expires=${expires};`;
-  const returnUrlEnd = _event.queryStringParameters?.redirect_url
-    ? decodeURIComponent(_event.queryStringParameters.redirect_url)
+  const returnUrlEnd = event.queryStringParameters?.redirect_url
+    ? decodeURIComponent(event.queryStringParameters.redirect_url)
     : '/';
   return {
     statusCode: 302,
