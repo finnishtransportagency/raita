cb0a5017ebb3e62e1d8db9bede2e73b8d51f1465
Stefan Rimaila <stefan@rimaila.fi>
2022-12-09 09:26:14 +0200

    New filtering controls (#62)
    * test(frontend): add missing tests, proper impl of toUndefined
    * feat(frontend): add new filtering controls
    * i18n(frontend): add missing localization keys
    * chore(frontend): finished filters functionality, extracted specific controls to own component
    * chore(frontend): update styles, index for filters
    * feat(frontend): implement new filters into reports
    * chore(frontend): remove dev styles
    * feat(frontend): add aria label prop for buttons
    * feat(frontend): use new filter controls
    * test(frontend): remove redundant commented-out lines
    * chore(frontend): clean up typedefs for filter controls
    * chore(frontend): clean up updateFilter
    * i18n(frontend): add missing filter reltypes
    * chore(frontend): removed redundant console.log

---
+++ frontend/components/filters/__tests__/selector.tsx
@@ -0,0 +1,91 @@
+import { fireEvent, logRoles, render } from '@testing-library/react';
+
+import Selector, { FieldDict } from '../selector';
+
+const fields: FieldDict = {
+  foo: { type: 'text' },
+  bar: { type: 'bool' },
+  top: { type: 'long' },
+  kek: { type: 'date' },
+};
+
+it('has a minimal working example', () => {
+  const callArgs = [
+    { field: '__EMPTY__' },
+    { field: 'inspection_date' },
+    { field: '__EMPTY__' },
+  ];
+
+  const fn = jest.fn();
+
+  render(
+    <Selector
+      fields={{
+        foo: { type: 'text' },
+        bar: { type: 'bool' },
+        top: { type: 'long' },
+        kek: { type: 'date' },
+      }}
+      filters={[
+        { field: 'foo', value: '' },
+        { field: 'bar', value: '' },
+        { field: 'top', value: '' },
+        { field: 'kek', value: '' },
+      ]}
+      onChange={fn}
+    />,
+  );
+});
+
+it('allows to add a filter', async () => {
+  const fn = jest.fn();
+
+  const { findAllByRole, queryAllByRole, getByLabelText } = render(
+    <Selector fields={{ foo: { type: 'text' } }} onChange={fn} />,
+  );
+
+  const filterCount = queryAllByRole('combobox').length;
+
+  expect(filterCount).toBe(0);
+
+  fireEvent.click(getByLabelText('common:add_filter'));
+
+  expect(await findAllByRole('combobox')).toHaveLength(filterCount + 1);
+});
+
+it('allows to update a filter', async () => {
+  const fn = jest.fn();
+
+  const { getByRole } = render(
+    <Selector
+      fields={{ foo: { type: 'text' } }}
+      filters={[{ field: 'foo', value: 'bar' }]}
+      onChange={fn}
+    />,
+  );
+
+  fireEvent.change(getByRole('textbox'), { target: { value: 'foo' } });
+
+  expect(fn.mock.calls[0][0]).toEqual([{ field: 'foo', value: 'bar' }]);
+  expect(fn.mock.calls[1][0]).toEqual([{ field: 'foo', value: 'foo' }]);
+});
+
+it('allows to remove a filter', async () => {
+  const fn = jest.fn();
+
+  const { getByRole } = render(
+    <Selector
+      fields={{ foo: { type: 'text' } }}
+      filters={[{ field: 'foo', value: 'bar' }]}
+      onChange={fn}
+    />,
+  );
+
+  // Ensure the initial state contains a filter
+  expect(fn).toHaveBeenLastCalledWith([{ field: 'foo', value: 'bar' }]);
+
+  fireEvent.click(getByRole('button', { name: 'common:remove_filter' }));
+
+  // After removing it, ensure it's actually removed
+  expect(fn).toHaveBeenLastCalledWith([]);
+});
