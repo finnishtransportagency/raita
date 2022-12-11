6f3bb13afb8924e616274b4b4b6c16fcbbcfcd03
Stefan Rimaila <stefan@rimaila.fi>
2022-12-11 22:19:48 +0200

    feat(frontend): give empty choice to dropdowns

--- frontend/components/dropdown/index.tsx
+++ frontend/components/dropdown/index.tsx
@@ -1,7 +1,11 @@
 import { clsx } from 'clsx';
+import { useTranslation } from 'next-i18next';
 import { SyntheticEvent } from 'react';
 
+import css from './dropdown.module.css';
+
 function Dropdown(props: Props) {
+  const { t } = useTranslation(['common']);
   const { label, items, disabled, onChange, multiple } = props;
 
   return (
@@ -11,13 +15,16 @@ function Dropdown(props: Props) {
       <select
         {...{ multiple, disabled, onChange }}
         className={clsx(
+          css.root,
           'block w-full border border-gray-500',
 
           // Add padding if this dropdown is single-select
           !multiple && 'p-2',
         )}
       >
-        <option className="p-2">Value 1</option>
+        <option className="p-2" value="">
+          {t('common:no_choice')}
+        </option>
         {items.map((it, ix) => {
           return (
             <option key={ix} value={it.value}>
