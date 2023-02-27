37f3fb065591e84aed1aaf300e0eaa0d3d487422
Arto Halonen <arto.halonen@hotmail.com>
2023-02-27 10:42:49 +0200

    feat: RAITA-278 Add instructions to UI
    New loading screen with contact information on fault- or access management
    related issues. New info banner showing the restrictions of the data being
    used.

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -7,6 +7,7 @@ export function Footer() {
     <footer className="bg-main-gray-75 text-white">
       <div className="container mx-auto px-16 py-12">
         <header className="text-lg">Väylävirasto</header>
+        <p>Käyttöoikeusasioissa ja ongelmatilanteisiin liittyen, ole yhteydessä raide@vayla.fi</p>
       </div>
     </footer>
   );
---
+++ frontend/components/loading-overlay.tsx
@@ -0,0 +1,24 @@
+import clsx from 'clsx';
+import css from './loading-overlay.module.css';
+
+export function LoadingOverlay() {
+  return (
+    <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
+      <div
+        className={clsx(
+          css.loader,
+          'ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4',
+        )}
+      ></div>
+      <h2 className="text-center text-white text-xl font-semibold">
+        Ladataan...
+      </h2>
+      <p className="w-1/3 text-center text-white">
+        Käyttöoikeusasioissa ja ongelmatilanteisiin liittyen, ole yhteydessä
+        raide@vayla.fi.
+      </p>
+    </div>
+  );
+}
+
+export default LoadingOverlay;
