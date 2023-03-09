6989507d0e7eec75df12c1871d20e7b71413df0a
Arto Halonen <45736486+Haloft@users.noreply.github.com>
2023-03-09 15:51:14 +0200

    fix: RAITA-200 Trigger zip process with lambda (#123)
    Call the zip processing lambda with another lambda, to get
    asynchronous functionality. Modified the priority due to cloudformation errors.
    New role and policy for zipRequest lambda to be able to invoke the zipProcess lambda
    removed console.logs

--- backend/lambdas/raitaApi/handleZipRequest/handleZipRequest.ts
+++ backend/lambdas/raitaApi/handleZipRequest/handleZipRequest.ts
@@ -1,117 +1,49 @@
-import {
-  GetObjectCommand,
-  PutObjectCommand,
-  S3Client,
-} from '@aws-sdk/client-s3';
-import { S3 } from 'aws-sdk';
 import { ALBEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
-import JSZip from 'jszip';
+import { LambdaClient, InvokeCommand } from '@aws-sdk/client-lambda';
 
 import { getGetEnvWithPreassignedContext } from '../../../../utils';
 import { log } from '../../../utils/logger';
 import { getUser, validateReadUser } from '../../../utils/userService';
 import {
-  shouldUpdateProgressData,
-  updateProgressFailed,
-  uploadProgressData,
-  validateInputs,
-} from './utils';
-import { initialProgressData, successProgressData } from './constants';
+  getRaitaLambdaErrorResponse,
+  getRaitaSuccessResponse,
+} from '../../utils';
+import { TextEncoder } from 'util';
 
 function getLambdaConfigOrFail() {
   const getEnv = getGetEnvWithPreassignedContext('handleZipRequest');
   return {
-    sourceBucket: getEnv('SOURCE_BUCKET'),
-    targetBucket: getEnv('TARGET_BUCKET'),
+    zipProcessingFn: getEnv('ZIP_PROCESSING_FN'),
+    region: getEnv('REGION'),
   };
 }
 
 export async function handleZipRequest(
   event: ALBEvent,
   _context: Context,
-): Promise<void> {
+): Promise<APIGatewayProxyResult> {
   const { body } = event;
   const requestBody = body && JSON.parse(body);
-  const { keys, pollingFileKey } = requestBody;
-  const s3Client = new S3Client({});
-  const zip = new JSZip();
+  const payloadString = JSON.stringify(requestBody);
+  const payloadBytes = new TextEncoder().encode(payloadString);
   try {
     const user = await getUser(event);
     await validateReadUser(user);
-    validateInputs(keys, pollingFileKey);
-    const { sourceBucket, targetBucket } = getLambdaConfigOrFail();
+    const { zipProcessingFn, region } = getLambdaConfigOrFail();
 
-    const totalKeys = keys.length;
-    uploadProgressData(initialProgressData, targetBucket, pollingFileKey);
-
-    let lastUpdateStep = 0;
-    let progressPercentage = 0;
-    const promises = keys.map(async (key: string, index: number) => {
-      const command = new GetObjectCommand({
-        Bucket: sourceBucket,
-        Key: key,
-      });
-      const data = await s3Client.send(command);
-      zip.file(key, data.Body as Blob);
-      progressPercentage = Math.floor(((index + 1) / totalKeys) * 100);
-      if (shouldUpdateProgressData(progressPercentage, lastUpdateStep)) {
-        uploadProgressData(
-          { ...initialProgressData, progressPercentage },
-          targetBucket,
-          pollingFileKey,
-        );
-        lastUpdateStep = progressPercentage;
-      }
-      return data;
+    const client = new LambdaClient({ region });
+    const command = new InvokeCommand({
+      FunctionName: zipProcessingFn,
+      Payload: payloadBytes,
+      InvocationType: 'Event',
     });
+    await client.send(command);
 
-    await Promise.all(promises).catch(err => {
-      log.error(`Error getting S3 Objects: ${err}`);
-      updateProgressFailed(targetBucket, pollingFileKey);
-      throw err;
+    return getRaitaSuccessResponse({
+      message: 'Zip processing initiated succesfully',
     });
-
-    const zipData = await zip
-      .generateAsync({ type: 'nodebuffer' })
-      .catch(err => {
-        log.error(`Error generating zip file: ${err}`);
-        updateProgressFailed(targetBucket, pollingFileKey);
-        throw err;
-      });
-
-    const destKey = `zip/raita-zip-${Date.now()}.zip`;
-    const putCommand = new PutObjectCommand({
-      Bucket: targetBucket,
-      Key: destKey,
-      Body: zipData,
-    });
-
-    await s3Client.send(putCommand).catch(err => {
-      log.error(`Error uploading zip file to S3: ${err}`);
-      updateProgressFailed(targetBucket, pollingFileKey);
-      throw err;
-    });
-
-    const s3 = new S3();
-    const url = await s3
-      .getSignedUrlPromise('getObject', {
-        Bucket: targetBucket,
-        Key: destKey,
-        Expires: 30,
-      })
-      .catch(err => {
-        log.error(
-          `Error getting the signed url for key: ${destKey} error: ${err}`,
-        );
-        throw err;
-      });
-
-    uploadProgressData(
-      { ...successProgressData, url },
-      targetBucket,
-      pollingFileKey,
-    );
-  } catch (err: unknown) {
-    log.error(err);
+  } catch (error) {
+    log.error(error);
+    return getRaitaLambdaErrorResponse(error);
   }
 }
