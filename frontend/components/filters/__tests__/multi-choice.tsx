d85986a02192f811acb1ec58e5f50e51cff195d2
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2023-09-19 13:17:35 +0300

    test: Unit test for filtering

--- frontend/components/filters/__tests__/multi-choice.tsx
+++ frontend/components/filters/__tests__/multi-choice.tsx
@@ -1,9 +1,37 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import MultiChoice from '../multi-choice';
 
 it('has a minimal working example', () => {
   const fn = jest.fn();
 
-  render(<MultiChoice items={[]} onChange={fn} />);
+  render(<MultiChoice items={[]} onChange={fn} resetFilters={false} />);
+});
+
+it('can filter selectable elements', async () => {
+  const fn = jest.fn();
+  const testItems = [
+    {
+      key: 'test1',
+      value: 'value1',
+    },
+    {
+      key: 'test2',
+      value: 'value2',
+    },
+    {
+      key: 'other1',
+      value: 'value3',
+    },
+  ];
+
+  const { findByText, getByPlaceholderText, queryByText } = render(
+    <MultiChoice items={testItems} onChange={fn} resetFilters={false} />,
+  );
+
+  const input = getByPlaceholderText('common:filter_options');
+  fireEvent.change(input, { target: { value: 'test' } });
+  expect(await findByText(testItems[0].key)).toBeTruthy();
+  expect(await findByText(testItems[1].key)).toBeTruthy();
+  expect(queryByText(testItems[2].key)).toBeFalsy();
 });
