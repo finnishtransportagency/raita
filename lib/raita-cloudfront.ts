fa78936b17d7398e55c90398bdffac4f38d7dd00
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2023-10-16 12:54:10 +0300

    fix: try fix for cloudfront stack ssm parameters

--- lib/raita-cloudfront.ts
+++ lib/raita-cloudfront.ts
@@ -4,7 +4,7 @@ import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Construct } from 'constructs';
-import { RaitaEnvironment } from './config';
+import { RaitaEnvironment, getRaitaStackConfig } from './config';
 import { isDevelopmentMainStack, isPermanentStack } from './utils';
 import { FrontendStack } from './raita-frontend';
 import {
@@ -22,23 +22,13 @@ interface CloudfrontStackProps extends StackProps {
   readonly raitaStackIdentifier: string;
   readonly raitaEnv: RaitaEnvironment;
   readonly stackId: string;
-  readonly cloudfrontCertificateArn: string;
-  readonly cloudfrontDomainName: string;
-  readonly dmzApiEndpoint: string;
 }
 
 // Based on: https://idanlupinsky.com/blog/static-site-deployment-using-aws-cloudfront-and-the-cdk/
 export class CloudfrontStack extends Stack {
   constructor(scope: Construct, id: string, props: CloudfrontStackProps) {
     super(scope, id, props);
-    const {
-      raitaStackIdentifier,
-      cloudfrontCertificateArn,
-      cloudfrontDomainName,
-      dmzApiEndpoint,
-      stackId,
-      raitaEnv,
-    } = props;
+    const { raitaStackIdentifier, stackId, raitaEnv } = props;
 
     // Create frontend stack to hold frontend artifacts
     const frontendStack = new FrontendStack(this, 'stack-fe', {
@@ -46,7 +36,10 @@ export class CloudfrontStack extends Stack {
       raitaStackIdentifier,
       stackId,
     });
-
+    const config = getRaitaStackConfig(this, raitaEnv);
+    const cloudfrontDomainName = config.cloudfrontDomainName;
+    const dmzApiEndpoint = config.dmzApiEndpoint;
+    const cloudfrontCertificateArn = config.cloudfrontCertificateArn;
     // Create Cloudfront itself conditionally - only for main and prod stackIds
     // Feature branches do not provide access from outside
     if (isPermanentStack(stackId, raitaEnv)) {
