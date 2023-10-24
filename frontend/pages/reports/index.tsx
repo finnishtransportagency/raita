be19eb313b6cfd9710c8408227bd28a92bc07880
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2023-10-24 13:49:15 +0300

    fix: allow scrolling the manual modal

--- frontend/pages/reports/index.tsx
+++ frontend/pages/reports/index.tsx
@@ -583,27 +583,20 @@ const ReportsIndex: NextPage = () => {
         onRequestClose={() => setManualModalOpen(false)}
         contentLabel={t('common:user_manual') || ''}
         appElement={document.getElementById('__next') || undefined}
-        className={clsx(css['manual-modal'])}
+        overlayClassName={clsx(css['info-modal-overlay'])}
+        bodyOpenClassName={clsx(css['info-modal-open'])}
+        className={clsx(css['info-modal'])}
       >
-        <div className={clsx(css['manual-header'])}>
+        <div className={clsx(css['info-header'])}>
           <button
-            className={clsx(css['manual-close-button'])}
+            className={clsx(css['info-close-button'])}
             onClick={() => setManualModalOpen(false)}
           >
             <CloseIcon />
-            {/* <svg
-              className="fill-current h-6 w-6 text-white-500"
-              role="button"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <title>{t('common:close')}</title>
-              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-            </svg> */}
           </button>
         </div>
         <div
-          className={clsx(css['manual-content'])}
+          className={clsx(css['info-content'])}
           dangerouslySetInnerHTML={{
             // TODO: do we need to sanitize?
             __html: marked.parse(manualData),
