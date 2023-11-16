d215a9a32135856c524c40674b9c612e0afbc984
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-11-16 14:19:29 +0200

    csv parser wip

---
+++ backend/lambdas/dataProcess/handleInspectionFileEvent/cvsDataParser/amsCsvDataParser.ts
@@ -0,0 +1,7 @@
+import { zcsv, parseCSVContent, parseCSV } from "zod-csv";
+import { z } from "zod";
+import {IFileResult} from "../../../../types";
+
+export async function parseAMSCSVData(file: IFileResult){
+
+}
