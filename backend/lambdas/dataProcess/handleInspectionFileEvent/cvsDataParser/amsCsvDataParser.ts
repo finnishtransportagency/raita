9cf8c0b6eafae108fe54afe7375d7a08f37d1a43
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-11-23 12:50:09 +0200

    csv parser wip

--- backend/lambdas/dataProcess/handleInspectionFileEvent/cvsDataParser/amsCsvDataParser.ts
+++ backend/lambdas/dataProcess/handleInspectionFileEvent/cvsDataParser/amsCsvDataParser.ts
@@ -1,25 +1,46 @@
-import { zcsv, parseCSVContent, parseCSV } from "zod-csv";
-import { z } from "zod";
+import { parseCSVContent }  from "../../../../utils/zod-csv/zcsv";
 import {amsSchema} from "./amsCsvSchema";
+import {log} from "../../../../utils/logger";
+
 
 function tidyUpHeaderLine(cvsHeaderLine: string):string {
-    return cvsHeaderLine.replace(' ','_').toLowerCase().replace(/\[.*\]/,'');
+  var a = cvsHeaderLine.replace(' ','_').toLowerCase();
+  console.log("a: " + a );
+  var b = a.replace(/\[.*?\]/g,'');
+  console.log("b: " + b);
+
+    return b;
 }
 
 function tidyUpFileBody(csvFileBody: string) {
     const firstNewLinePos = csvFileBody.search(/\r\n|\r|\n/);
-    //trash first line; csv headears are on the second
-    const secondNewLinePos = csvFileBody.slice(firstNewLinePos).search(/\r\n|\r|\n/);
+  console.log("firstNewLinePos: " + firstNewLinePos);
 
-    const cvsHeaderLine = csvFileBody.slice(firstNewLinePos, secondNewLinePos);
-    const cvsDataLines = csvFileBody.slice(secondNewLinePos);
+    //trash first line; csv headears are on the second
+  const bodyWithoutFirstLIne = csvFileBody.slice(firstNewLinePos+1);
+  console.log("bodyWithoutFirstLIne: " + bodyWithoutFirstLIne);
+    const secondNewLinePos = bodyWithoutFirstLIne.search(/\r\n|\r|\n/);
+  console.log("secondNewLinePos: " + secondNewLinePos);
+    const cvsHeaderLine = bodyWithoutFirstLIne.slice(0, secondNewLinePos);
+  console.log("cvsHeaderLine: " + cvsHeaderLine);
+
+    const cvsDataLines = bodyWithoutFirstLIne.slice(secondNewLinePos);
+  console.log("cvsDataLines: " + cvsDataLines);
+
+
+  var b = tidyUpHeaderLine(cvsHeaderLine);
+  console.log("tidyHeaderLine: " + b);
 
     return tidyUpHeaderLine(cvsHeaderLine).concat(cvsDataLines);
 }
 
 export async function parseAMSCSVData(csvFileBody: string){
     const tidyedFileBody = tidyUpFileBody(csvFileBody);
-    parseCSV(tidyedFileBody, amsSchema);
+    console.log(tidyedFileBody);
+
+   var a = parseCSVContent(tidyedFileBody, amsSchema);
+   console.log("parsed data: " + a);
+  console.log(a);
 
 
 }
