ee3fee04d2dc427bfc6d06c4dbd602a0f66c5a8f
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-11-28 17:23:39 +0200

    csv parser wip; test error

--- backend/lambdas/dataProcess/handleInspectionFileEvent/__tests__/csvDataParser.test.ts
+++ backend/lambdas/dataProcess/handleInspectionFileEvent/__tests__/csvDataParser.test.ts
@@ -10,10 +10,28 @@ const amsCsv =
   '318107,"008 KOKOL LR",630+0851.00,"64.07647756° N","24.54062880° E",55.924,-21.2956,21.9569,14.8956,12.2093,8.3666,7.9174,-3.6308,4.0280,2.7983,2.0624,1.3882,1.7930,2.9541,2.8539,-2.6747,-1.8791,-1.2058,1.6617,-0.3302,1.8971,0.8156,0.1039,56\n' +
   '318108,"008 KOKOL LR",630+0851.25,"64.07647979° N","24.54062875° E",55.925,-23.1085,25.8125,-21.9575,-12.1894,8.3251,7.8952,3.0163,-4.6372,2.5206,-2.8624,1.3800,1.8019,3.2062,3.0976,-3.2225,-1.4226,-2.0412,1.6744,-0.3927,2.0022,0.7377,0.0205,56';
 
-describe('parseAMSCSV success', () => {
+const amsCsvError =
+  '"Running Date","22/11/2022 7:44:40 AM"\n' +
+  '"SSCount","Track","Location [km+m]","Latitude","Longitude","Ajonopeus [Km/h]","Running Dynamics.Vasen Pystysuuntainen Kiihtyvyys C1 [m/s^2]","Running Dynamics.Oikea Pystysuuntainen Kiihtyvyys C1 Suodatettu [m/s^2]","Running Dynamics.Vasen Pystysuuntainen Kiihtyvyys C1 Suodatettu [m/s^2]","Running Dynamics.Oikea Pystysuuntainen Kiihtyvyys C1 Keskihajonta [m/s^2]","Running Dynamics.Vasen Pystysuuntainen Kiihtyvyys C1 Keskihajonta [m/s^2]","Running Dynamics.Oikea Poikittainen Kiihtyvyys C1 [m/s^2]","Running Dynamics.Vasen Poikittainen Kiihtyvyys C1 [m/s^2]","Running Dynamics.Oikea Poikittainen Kiihtyvyys C1 Suodatettu [m/s^2]","Running Dynamics.Vasen Poikittainen Kiihtyvyys C1 Suodatettu [m/s^2]","Running Dynamics.Oikea Poikittainen Kiihtyvyys C1 Keskihajonta [m/s^2]","Running Dynamics.Vasen Poikittainen Kiihtyvyys C1 Keskihajonta [m/s^2]","Running Dynamics.Pystysuuntainen Kiihtyvyys C2 [m/s^2]","Running Dynamics.Pystysuuntainen Kiihtyvyys C2 Suodatettu [m/s^2]","Running Dynamics.Poikittainen Kiihtyvyys C2 [m/s^2]","Running Dynamics.Poikittainen Kiihtyvyys C2 Suodatettu [m/s^2]","Running Dynamics.Transversal Acceleration C2 Mean-to-Peak [m/s^2]","Running Dynamics.Pystysuuntainen Kiihtyvyys C3 [m/s^2]","Running Dynamics.Pystysuuntainen Kiihtyvyys C3 Suodatettu [m/s^2]","Running Dynamics.Poikittainen Kiihtyvyys C3 [m/s^2]","Running Dynamics.Poikittainen Kiihtyvyys C3 Suodatettu [m/s^2]","Running Dynamics.Transversal Acceleration C3 Mean-to-Peak [m/s^2]","Running Dynamics.Ajonopeus [Km/h]"\n' +
+  '318103,"008 KOKOL LR",630+0850.00,"64.07646857° N","24.54062901° E",55.985, 26.3496,14.6794,14.1478,8.1315,8.0237,-4.1229,-6.3282,3.1816,-3.5781,1.3801,1.7761,2.2629,2.1137,-4.7717,-2.7778,-1.3045,1.3953,0.5937,1.2821,0.5037,0.3869,56\n';
+
+
+  describe('parseAMSCSV success', () => {
   test('success: normal run', async () => {
     const result = await parseAMSCSVData(amsCsv);
     expect(result.success).toBe(true);
+    expect(result.header[6]).toBe('oikea_pystysuuntainen_kiihtyvyys_c1');
+    expect(result.validRows[5].oikea_poikittainen_kiihtyvyys_c1).toBe(3.0163);
+    expect(result.validRows.length).toBe(6);
+    expect(result.allRows.length).toBe(6);
+  });
+});
+
+describe('parseAMSCSV error', () => {
+  test('success: normal run', async () => {
+    const result = await parseAMSCSVData(amsCsvError);
+    expect(result.success).toBe(false);
+    expect(result.errors).toBe(false);
     expect(result.header[6]).toBe('oikea_pystysuuntainen_kiihtyvyys_c1');
     expect(result.validRows[5].oikea_poikittainen_kiihtyvyys_c1).toBe(3.0163);
     expect(result.validRows.length).toBe(6);
