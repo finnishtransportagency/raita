2ebed3f8866f548bc4d4cc05acbba20b3c5d5945
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-11-28 14:44:19 +0200

    csv parser wip; tidy header line

--- backend/lambdas/dataProcess/handleInspectionFileEvent/cvsDataParser/amsCsvDataParser.ts
+++ backend/lambdas/dataProcess/handleInspectionFileEvent/cvsDataParser/amsCsvDataParser.ts
@@ -1,46 +1,47 @@
-import { parseCSVContent }  from "../../../../utils/zod-csv/zcsv";
-import {amsSchema} from "./amsCsvSchema";
-import {log} from "../../../../utils/logger";
+import { parseCSVContent } from '../../../../utils/zod-csv/zcsv';
+import { amsSchema } from './amsCsvSchema';
+import { log } from '../../../../utils/logger';
 
-
-function tidyUpHeaderLine(cvsHeaderLine: string):string {
-  var a = cvsHeaderLine.replace(' ','_').toLowerCase();
-  console.log("a: " + a );
-  var b = a.replace(/\[.*?\]/g,'');
-  console.log("b: " + b);
-
-    return b;
+// tidy up csv header line so headers are correct form for validating
+function tidyUpHeaderLine(cvsHeaderLine: string): string {
+  //replace spaces with underscore
+  var a = cvsHeaderLine.replace(/ /g, '_').toLowerCase();
+  console.log('a: ' + a);
+  //remove square braqeted parts
+  var b = a.replace(/\[.*?\]/g, '');
+  console.log('b: ' + b);
+  //remove trailing underscores
+  var c = b.replace(/_\"/g, '"');
+  console.log('c: ' + c);
+  return c;
 }
 
 function tidyUpFileBody(csvFileBody: string) {
-    const firstNewLinePos = csvFileBody.search(/\r\n|\r|\n/);
-  console.log("firstNewLinePos: " + firstNewLinePos);
+  const firstNewLinePos = csvFileBody.search(/\r\n|\r|\n/);
+  console.log('firstNewLinePos: ' + firstNewLinePos);
 
-    //trash first line; csv headears are on the second
-  const bodyWithoutFirstLIne = csvFileBody.slice(firstNewLinePos+1);
-  console.log("bodyWithoutFirstLIne: " + bodyWithoutFirstLIne);
-    const secondNewLinePos = bodyWithoutFirstLIne.search(/\r\n|\r|\n/);
-  console.log("secondNewLinePos: " + secondNewLinePos);
-    const cvsHeaderLine = bodyWithoutFirstLIne.slice(0, secondNewLinePos);
-  console.log("cvsHeaderLine: " + cvsHeaderLine);
+  //trash first line; csv headears are on the second
+  const bodyWithoutFirstLIne = csvFileBody.slice(firstNewLinePos + 1);
+  console.log('bodyWithoutFirstLIne: ' + bodyWithoutFirstLIne);
+  const secondNewLinePos = bodyWithoutFirstLIne.search(/\r\n|\r|\n/);
+  console.log('secondNewLinePos: ' + secondNewLinePos);
+  const cvsHeaderLine = bodyWithoutFirstLIne.slice(0, secondNewLinePos);
+  console.log('cvsHeaderLine: ' + cvsHeaderLine);
 
-    const cvsDataLines = bodyWithoutFirstLIne.slice(secondNewLinePos);
-  console.log("cvsDataLines: " + cvsDataLines);
-
+  const cvsDataLines = bodyWithoutFirstLIne.slice(secondNewLinePos);
+  console.log('cvsDataLines: ' + cvsDataLines);
 
   var b = tidyUpHeaderLine(cvsHeaderLine);
-  console.log("tidyHeaderLine: " + b);
+  console.log('tidyHeaderLine: ' + b);
 
-    return tidyUpHeaderLine(cvsHeaderLine).concat(cvsDataLines);
+  return tidyUpHeaderLine(cvsHeaderLine).concat(cvsDataLines);
 }
 
-export async function parseAMSCSVData(csvFileBody: string){
-    const tidyedFileBody = tidyUpFileBody(csvFileBody);
-    console.log(tidyedFileBody);
+export async function parseAMSCSVData(csvFileBody: string) {
+  const tidyedFileBody = tidyUpFileBody(csvFileBody);
+  console.log(tidyedFileBody);
 
-   var a = parseCSVContent(tidyedFileBody, amsSchema);
-   console.log("parsed data: " + a);
+  var a = parseCSVContent(tidyedFileBody, amsSchema);
+  console.log('parsed data: ' + a);
   console.log(a);
-
-
 }
