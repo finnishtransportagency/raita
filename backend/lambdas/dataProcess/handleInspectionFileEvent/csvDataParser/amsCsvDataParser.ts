dbfbc4ed742871471bf78758361db7542a27753f
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-11-28 15:09:01 +0200

    csv parser wip; tidy ams header

--- backend/lambdas/dataProcess/handleInspectionFileEvent/csvDataParser/amsCsvDataParser.ts
+++ backend/lambdas/dataProcess/handleInspectionFileEvent/csvDataParser/amsCsvDataParser.ts
@@ -2,9 +2,17 @@ import { parseCSVContent } from '../../../../utils/zod-csv/zcsv';
 import { amsSchema } from './amsCsvSchema';
 import { tidyUpFileBody } from './utils';
 
+function tidyHeadersAMSSpecific(headerLine: string):string{
+  var c = headerLine.replace('running_dynamics.', '');
+  console.log('c: ' + c);
+  return c;
+}
+
 export async function parseAMSCSVData(csvFileBody: string) {
-  const tidyedFileBody = tidyUpFileBody(csvFileBody);
+  const tidyedFileBody = tidyUpFileBody(csvFileBody, tidyHeadersAMSSpecific);
   console.log(tidyedFileBody);
+
+
 
   var a = parseCSVContent(tidyedFileBody, amsSchema);
   console.log('parsed data: ' + a);
