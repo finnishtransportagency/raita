7c38acc119e01081e926e82ed412eaa8b80fd63c
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2023-12-05 17:08:11 +0200

    feat: improvements to delete confirmation modal

--- frontend/pages/admin/delete.tsx
+++ frontend/pages/admin/delete.tsx
@@ -16,22 +16,15 @@ import { DeleteResponse } from 'shared/types';
 const DeleteIndex: NextPage = () => {
   const { t } = useTranslation(['common', 'admin']);
 
-  const [prefixInput, setInputText] = useState('');
+  const [prefixInput, setPrefixInput] = useState('');
+  const [confirmInput, setConfirmInput] = useState('');
   const [confirmModalOpen, setConfirmModalOpen] = useState(false);
   const [resultModalOpen, setResultModalOpen] = useState(false);
   const [deleteResult, setDeleteResult] = useState<DeleteResponse | null>(null);
   const [deleteInProgress, setDeleteInProgress] = useState(false);
+  const [deleteIsConfirmed, setDeleteIsConfirmed] = useState(false);
 
   const filesToDeleteMutation = useSearch();
-
-  const resetAll = () => {
-    filesToDeleteMutation.reset();
-    setConfirmModalOpen(false);
-    setResultModalOpen(false);
-    setInputText('');
-    setDeleteResult(null);
-    setDeleteInProgress(false);
-  };
 
   const fetchFilesToBeDeleted = () => {
     const query = makeQuery(
@@ -47,6 +40,9 @@ const DeleteIndex: NextPage = () => {
   };
 
   const deleteFiles = async () => {
+    if (!deleteIsConfirmed) {
+      return;
+    }
     setDeleteInProgress(true);
     try {
       const response = await postDeleteRequest(prefixInput);
@@ -61,8 +57,36 @@ const DeleteIndex: NextPage = () => {
     }
   };
 
+  const onCancelConfirmation = () => {
+    if (deleteInProgress) {
+      return;
+    }
+    filesToDeleteMutation.reset();
+    setConfirmModalOpen(false);
+    setDeleteIsConfirmed(false);
+    setConfirmInput('');
+    setDeleteInProgress(false);
+  };
+
+  const resetAll = () => {
+    onCancelConfirmation();
+    setResultModalOpen(false);
+    setPrefixInput('');
+    setDeleteResult(null);
+  };
+
+  const onConfirmInputChange = (inputText: string) => {
+    setConfirmInput(inputText);
+    if (inputText === filesToDeleteMutation.data?.total.toString()) {
+      setDeleteIsConfirmed(true);
+    } else {
+      setDeleteIsConfirmed(false);
+    }
+  };
+
   const readyToDelete =
     confirmModalOpen &&
+    deleteIsConfirmed &&
     filesToDeleteMutation.data &&
     filesToDeleteMutation.data.total > 0 &&
     !deleteInProgress;
@@ -76,7 +100,7 @@ const DeleteIndex: NextPage = () => {
         }}
       />
       <TextInput
-        onUpdate={setInputText}
+        onUpdate={setPrefixInput}
         value={prefixInput}
         placeholder={t('admin:delete_input_placeholder') || ''}
         resetSearchText={false}
@@ -89,13 +113,14 @@ const DeleteIndex: NextPage = () => {
       />
       <Modal
         isOpen={confirmModalOpen}
-        onRequestClose={() => setConfirmModalOpen(false)}
+        onRequestClose={() => onCancelConfirmation()}
         contentLabel={t('admin:delete_confirm_heading') || ''}
         headerText={t('admin:delete_confirm_heading') || ''}
       >
         <div>
           <p>{t('admin:delete_path_info', { path: prefixInput })}</p>
           {filesToDeleteMutation.isLoading && t('admin:delete_files_loading')}
+          {!!filesToDeleteMutation.error && t('admin:delete_files_fetch_error')}
           {filesToDeleteMutation.data &&
             (filesToDeleteMutation.data.total === 0 ? (
               <p>{t('admin:delete_files_not_found')}</p>
@@ -106,7 +131,16 @@ const DeleteIndex: NextPage = () => {
                     count: filesToDeleteMutation.data.total,
                   })}
                 </p>
-                <p>{t('admin:delete_confirmation')}</p>
+                <label>
+                  {t('admin:delete_confirmation')}
+                  <TextInput
+                    onUpdate={onConfirmInputChange}
+                    value={confirmInput}
+                    placeholder={t('admin:delete_confirm_placeholder') || ''}
+                    resetSearchText={false}
+                    className={clsx(css.textInput)}
+                  />
+                </label>
               </>
             ))}
         </div>
@@ -117,7 +151,8 @@ const DeleteIndex: NextPage = () => {
           label={t('admin:delete_label')}
         />
         <Button
-          onClick={() => setConfirmModalOpen(false)}
+          onClick={() => onCancelConfirmation()}
+          disabled={deleteInProgress}
           label={t('common:cancel')}
         />
         {deleteInProgress && <p>{t('admin:delete_in_progress')}</p>}
