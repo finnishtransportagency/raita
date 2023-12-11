927c5ef53c4898ec8c5f40814cc090f8dc783371
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-12-11 12:38:23 +0200

    csv parser wip; raportti table insert

--- backend/lambdas/dataProcess/handleInspectionFileEvent/csvDataParser/csvDataParser.ts
+++ backend/lambdas/dataProcess/handleInspectionFileEvent/csvDataParser/csvDataParser.ts
@@ -1,19 +1,59 @@
-import {IFileResult} from "../../../../types";
-import {log} from "../../../../utils/logger";
-import {parseAMSCSVData} from "./amsCsvDataParser";
+import { IFileResult, ParseValueResult } from '../../../../types';
+import { log } from '../../../../utils/logger';
+import { parseAMSCSVData } from './amsCsvDataParser';
+import { RaporttiDBSchema } from './raporttiDBSchema';
+import { getDBConnection } from './dbUtil';
 
-export async function parseCSVData(fileBaseName: string, file: IFileResult)  {
-    const fileNameParts = fileBaseName.split('_');
-    const fileNamePrefix = fileNameParts[0];
+//todo get all needed values from metadata
+async function insertRaporttiData(
+  fileBaseName: string,
+  fileNamePrefix: string,
+  metadata: ParseValueResult,
+) {
+  const data: RaporttiDBSchema = {
+    zip_tiedostonimi: fileBaseName,
+    zip_vastaanotto_pvm: new Date(),
+    zip_vastaanotto_vuosi: new Date(),
+    pvm: new Date(),
+    vuosi: new Date(),
+  };
 
-    switch (fileNamePrefix){
-        case 'AMS':
-            file.fileBody && await parseAMSCSVData(file.fileBody);
-            break;
-        default:
-            log.warn('Unknown csv file prefix: ' + fileNamePrefix);
-            return 'fail';
-    }
-    return 'success';
+  let { schema, sql } = await getDBConnection();
 
+  try {
+    const reportID = await sql`INSERT INTO ${sql(schema)}.ams_mittaus ${sql(
+      data,
+    )} returning id`;
+    console.log(reportID);
+    return reportID;
+  } catch (e) {
+    console.log('err');
+    console.log(e);
+    return null;
+  }
+}
+
+export async function parseCSVData(
+  fileBaseName: string,
+  file: IFileResult,
+  metadata: ParseValueResult,
+) {
+  const fileNameParts = fileBaseName.split('_');
+  const fileNamePrefix = fileNameParts[0];
+
+  switch (fileNamePrefix) {
+    case 'AMS':
+      const reportId  = await insertRaporttiData(
+        fileBaseName,
+        fileNamePrefix,
+        metadata,
+      );
+      //consi id = reportId.
+      file.fileBody && (await parseAMSCSVData(file.fileBody));
+      break;
+    default:
+      log.warn('Unknown csv file prefix: ' + fileNamePrefix);
+      return 'fail';
+  }
+  return 'success';
 }
