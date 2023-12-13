18f9260f95dd0da374ddba048d8e692a4adc6475
Kalle Mustonen <kalle.mustonen@cgi.com>
2023-12-13 14:42:35 +0200

    csv parser wip;

--- backend/lambdas/dataProcess/handleInspectionFileEvent/csvDataParser/dbUtil.ts
+++ backend/lambdas/dataProcess/handleInspectionFileEvent/csvDataParser/dbUtil.ts
@@ -1,7 +1,8 @@
 import postgres from 'postgres';
 import { getEnvOrFail } from '../../../../../utils';
 import { getSecretsManagerSecret } from '../../../../utils/secretsManager';
-
+import { Mittaus } from './Mittaus';
+import { Rataosoite } from './rataosoite';
 
 let connection: postgres.Sql;
 //todo set false
@@ -44,7 +45,7 @@ async function getConnection() {
     return connection;
   }
   const password = await getSecretsManagerSecret('database_password');
-  connection = postgres({ password, transform: { undefined: null } }, );
+  connection = postgres({ password, transform: { undefined: null } });
   return connection;
 }
 
@@ -53,11 +54,60 @@ export async function getConnectionLocalDev() {
     return connection;
   }
   const password = 'password';
-  connection = postgres({ password, username: 'postgres', transform: { undefined: null } });
+  connection = postgres({
+    password,
+    username: 'postgres',
+    transform: { undefined: null },
+  });
   return connection;
 }
 
-export function convertToDBRow(row: any, runningDate: Date, reportId: number) {
+function constructRataosoite(track: string, location: string): Rataosoite {
+  //Track: "008 KOKOL LR"
+  //Location: "630+0850.00"
 
-  return { ...row, raportti_id: reportId, running_date: runningDate };
+  /*  rataosuus_numero character varying(40), -- for example '006'
+  rataosuus_nimi character varying(40), -- for example 'LHRP'
+  raide_numero integer, -- for example 2
+  rata_kilometri integer, -- for example 130
+  rata_metrit DECIMAL -- for example 100.00*/
+
+  const splittedTrack = track.split(' ');
+  let raideNumero = '';
+  let rataosuusNimi = '';
+  let rataosuusNumero = '';
+  if (splittedTrack.length == 3) {
+    rataosuusNumero = splittedTrack[0];
+    rataosuusNimi = splittedTrack[1];
+    raideNumero =  splittedTrack[2];
+  }
+
+  const splittedLocation = location.split('+');
+  let rataKilometri = null;
+  let rataMetrit = null;
+  try{
+    rataKilometri =  Number(splittedLocation[0]);
+    rataMetrit = Number(splittedLocation[1]);
+  }
+  catch (e) {
+    throw Error("Illegal location: " + location);
+  }
+
+  const rataosoite: Rataosoite = {
+    raide_numero: raideNumero,
+    rata_kilometri: rataKilometri,
+    rata_metrit: rataMetrit,
+    rataosuus_nimi: rataosuusNimi,
+    rataosuus_numero: rataosuusNumero,
+  };
+  return rataosoite;
+}
+
+export function convertToDBRow(
+  row: Mittaus,
+  runningDate: Date,
+  reportId: number,
+) {
+  const rataosoite: Rataosoite = constructRataosoite(row.track, row.location);
+  return { ...row, raportti_id: reportId, running_date: runningDate , ...rataosoite};
 }
