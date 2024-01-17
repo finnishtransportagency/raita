ba9e80d4a06d8b068c7238466bff91359aaf2600
Kalle Mustonen <kalle.mustonen@cgi.com>
2024-01-17 15:42:50 +0200

    handle missing running date line

--- backend/lambdas/dataProcess/handleInspectionFileEvent/csvDataParser/csvConversionUtils.ts
+++ backend/lambdas/dataProcess/handleInspectionFileEvent/csvDataParser/csvConversionUtils.ts
@@ -1,12 +1,11 @@
 // tidy up csv header line so headers are correct form for validating
 import { log } from '../../../../utils/logger';
-import {ZodObject} from "zod";
+import { ZodObject } from 'zod';
 
 function tidyUpDataLines(csvDataLines: string): string {
-  var tidyedLines= csvDataLines
-      .replace(/NaN/g, '')
-  log.info("tidyedLines: " + tidyedLines);
-  return tidyedLines
+  var tidyedLines = csvDataLines.replace(/NaN/g, '');
+  log.info('tidyedLines: ' + tidyedLines);
+  return tidyedLines;
 }
 
 function tidyUpHeaderLine(csvHeaderLine: string): string {
@@ -50,26 +49,26 @@ function tidyUpHeaderLine(csvHeaderLine: string): string {
 export function readRunningDate(csvFileBody: string) {
   const lines = csvFileBody.split('\n');
   const firstLine = lines[0];
-  log.info("firstLine: " + firstLine);
+  log.info('firstLine: ' + firstLine);
   const found = firstLine.search('Running Date');
   if (found == -1) {
-    log.error('Running date not in file first line');
+    log.warn('Running date not in file first line');
     return new Date();
   }
   const firstLineSplitted = firstLine.split(',');
-  log.info("1");
+  log.info('1');
   const runningDateString = firstLineSplitted[1];
-  log.info("2");
+  log.info('2');
   const runningDateStringDatePart = runningDateString.split(' ')[0];
-  log.info("3");
+  log.info('3');
   const splitted = runningDateStringDatePart.split('/');
-  log.info("4");
+  log.info('4');
   const day = splitted[0].substring(1);
-  log.info("5");
+  log.info('5');
   const month = splitted[1];
-  log.info("6");
+  log.info('6');
   const year = splitted[2];
-  log.info("7");
+  log.info('7');
   const runningDate = new Date(Number(year), Number(month), Number(day));
   console.log(runningDate);
   return runningDate;
@@ -78,24 +77,29 @@ export function readRunningDate(csvFileBody: string) {
 export function tidyUpFileBody(csvFileBody: string) {
   // const firstNewLinePos = csvFileBody.search(/\r\n|\r|\n/);
   const firstNewLinePos = csvFileBody.search(/\r\n/);
+  const firstLine = csvFileBody.slice(0, firstNewLinePos);
+  let fileBody = csvFileBody;
+  if (firstLine.search('Running Date') != -1) {
+    //trash first line; csv headears are on the second
+    fileBody = csvFileBody.slice(firstNewLinePos + 2);
+  }
 
-  //trash first line; csv headears are on the second
-  const bodyWithoutFirstLIne = csvFileBody.slice(firstNewLinePos + 2);
-  const secondNewLinePos = bodyWithoutFirstLIne.search(/\r\n|\r|\n/);
-  const csvHeaderLine = bodyWithoutFirstLIne.slice(0, secondNewLinePos);
+  const secondNewLinePos = fileBody.search(/\r\n|\r|\n/);
+  const csvHeaderLine = fileBody.slice(0, secondNewLinePos);
   log.info('csvHeaderLine: ' + csvHeaderLine);
-  const csvDataLines = bodyWithoutFirstLIne.slice(secondNewLinePos);
+  const csvDataLines = fileBody.slice(secondNewLinePos);
   let tidyHeaderLine = tidyUpHeaderLine(csvHeaderLine);
   let tidyDataLines = tidyUpDataLines(csvDataLines);
   //TODO make more generic to any missing colmumn?
-  log.info("tidyHeaderLine.substring(0,6)" + tidyHeaderLine.substring(0,6));
-  if(tidyHeaderLine.substring(0,6)!="sscount"){
-    tidyHeaderLine = "sscount," + tidyHeaderLine;
-    log.info("tidyHeaderLine.substring(0,6)" + tidyHeaderLine.substring(0,15));
-    tidyDataLines.replace(/\n/,'\n,');
-    log.info("tidyDataLines.substring(0,1000)" + tidyDataLines.substring(0,1000));
+  log.info('tidyHeaderLine.substring(0,6)' + tidyHeaderLine.substring(0, 6));
+  if (tidyHeaderLine.substring(0, 6) != 'sscount') {
+    tidyHeaderLine = 'sscount,' + tidyHeaderLine;
+    log.info('tidyHeaderLine.substring(0,6)' + tidyHeaderLine.substring(0, 15));
+    tidyDataLines.replace(/\n/, '\n,');
+    log.info(
+      'tidyDataLines.substring(0,1000)' + tidyDataLines.substring(0, 1000),
+    );
   }
 
   return tidyHeaderLine.concat(tidyDataLines);
-
 }
