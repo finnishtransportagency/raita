0aca368b7d0c4073a83b502dbe49dea987ec8403
Kalle Mustonen <kalle.mustonen@cgi.com>
2024-03-25 13:49:48 +0200

    disable mittaus db wrote for testing

--- backend/lambdas/dataProcess/handleCSVFileEvent/csvDataParser/csvDataParser.ts
+++ backend/lambdas/dataProcess/handleCSVFileEvent/csvDataParser/csvDataParser.ts
@@ -67,7 +67,8 @@ async function parseCsvAndWriteToDb(
     );
 
     try {
-      return await writeRowsToDB(dbRows, table, dbConnection);
+      //return await writeRowsToDB(dbRows, table, dbConnection);
+      return;
     } catch (e) {
       log.error('Error writing to db');
       log.error(e);
