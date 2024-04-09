048f65c37aac892c9b771a2d555b08d8e897552b
Kalle Mustonen <kalle.mustonen@cgi.com>
2024-04-09 12:47:55 +0300

    allow string track_id in db table raportti

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   //TODO some smart way to do db writing tests in dev
   testPathIgnorePatterns: [
     '/node_modules/',
-    //'csvDataParser',
+    'csvDataParser',
     'csvDataChopper',
     'contentDataParser',
     'parseFileMetaData',
