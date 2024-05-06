c8dd571a08051ff353bfe5a34c584963999183b1
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2024-05-06 13:30:41 +0300

    feat: check user roles hierarchically

---
+++ backend/utils/__tests__/userService.test.ts
@@ -0,0 +1,36 @@
+import {
+  RaitaUser,
+  validateAdminUser,
+  validateExtendedUser,
+  validateReadUser,
+} from '../userService';
+
+describe('userService role checks', () => {
+  test('success: read user', () => {
+    const readUser: RaitaUser = {
+      uid: 'read',
+      roles: ['Raita_luku'],
+    };
+    expect(validateReadUser(readUser)).resolves.not.toThrow();
+    expect(validateExtendedUser(readUser)).rejects.toBeTruthy();
+    expect(validateAdminUser(readUser)).rejects.toBeTruthy();
+  });
+  test('success: extended user user', () => {
+    const extendedUser: RaitaUser = {
+      uid: 'extended',
+      roles: ['Raita_extended'],
+    };
+    expect(validateReadUser(extendedUser)).resolves.not.toThrow();
+    expect(validateExtendedUser(extendedUser)).resolves.not.toThrow();
+    expect(validateAdminUser(extendedUser)).rejects.toBeTruthy();
+  });
+  test('success: extended user user', () => {
+    const adminUser: RaitaUser = {
+      uid: 'admin',
+      roles: ['Raita_admin'],
+    };
+    expect(validateReadUser(adminUser)).resolves.not.toThrow();
+    expect(validateExtendedUser(adminUser)).resolves.not.toThrow();
+    expect(validateAdminUser(adminUser)).resolves.not.toThrow();
+  });
+});
