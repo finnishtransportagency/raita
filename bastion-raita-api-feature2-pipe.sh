3b4b207be280832354fe27791f820c688509dfb7
KPuranen <kepura@utu.fi>
2024-06-10 11:08:33 +0300

    deleted extra file

--- bastion-raita-api-feature2-pipe.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source ./loadbastionenv
-aws ssm start-session --target $INSTANCE_ID --profile $PROFILE --region eu-west-1 --document-name AWS-StartPortForwardingSession --parameters '{"portNumber":["3004"], "localPortNumber":["3004"]}'
