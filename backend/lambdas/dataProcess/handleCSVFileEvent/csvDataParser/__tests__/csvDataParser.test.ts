2a4570e43a28d80af2edf261562f08b60b20a98a
Kalle Mustonen <kalle.mustonen@cgi.com>
2024-06-13 11:33:44 +0300

    tests to actually test field replacement success

--- backend/lambdas/dataProcess/handleCSVFileEvent/csvDataParser/__tests__/csvDataParser.test.ts
+++ backend/lambdas/dataProcess/handleCSVFileEvent/csvDataParser/__tests__/csvDataParser.test.ts
@@ -6,10 +6,11 @@ import {
 import * as fs from 'fs';
 import { stringToStream } from '../../../handleInspectionFileEvent/__tests__/testUtils';
 import {
+  DBConnection,
   DBUtil
 } from '../../../csvCommon/db/dbUtil';
 import { z } from 'zod';
-import { parseCSVContent, zcsv } from '../../../../../../external/zod-csv/zcsv';
+import { parseCSVContent, zcsv } from '../../../../../../external/zod-csv';
 import { amsSchema } from '../csvSchemas/amsCsvSchema';
 
 const ohlWithSurveyDate =
@@ -66,6 +67,17 @@ const amsCsv =
 const amsCsvStream = stringToStream(amsCsv);
 //const amsCsvStream = fs.createReadStream('./backend/lambdas/dataProcess/handleCSVFileEvent/csvDataParser/__tests__/AMS_20221024_251_LHRP_1_130_131.csv');
 //const amsCsvStream = fs.createReadStream('./backend/lambdas/dataProcess/handleCSVFileEvent/csvDataParser/__tests__/chunkFile_4879_1_AMS_20230605_222_HMARP_002_242_243.csv');
+
+const amsCsvWithUnkownMisspelledField =
+  '"Running Date","22/11/2022 7:44:40 AM"\r\n' +
+  '"SSCount","Track","Location [km+m]","Latitude","Longitude","Ajonopeus [Km/h]","Running Dynamics.Oikea Pystysuuntainen Kiityvyys C1 [m/s^2]","Running Dynamics.Vasen Pystysuuntainen Kiihtyvyys C1 [m/s^2]","Running Dynamics.Oikea Pystysuuntainen Kiihtyvyys C1 Suodatettu [m/s^2]","Running Dynamics.Vasen Pystysuuntainen Kiihtyvyys C1 Suodatettu [m/s^2]","Running Dynamics.Oikea Pystysuuntainen Kiihtyvyys C1 Keskihajonta [m/s^2]","Running Dynamics.Vasen Pystysuuntainen Kiihtyvyys C1 Keskihajonta [m/s^2]","Running Dynamics.Oikea Poikittainen Kiihtyvyys C1 [m/s^2]","Running Dynamics.Vasen Poikittainen Kiihtyvyys C1 [m/s^2]","Running Dynamics.Oikea Poikittainen Kiihtyvyys C1 Suodatettu [m/s^2]","Running Dynamics.Vasen Poikittainen Kiihtyvyys C1 Suodatettu [m/s^2]","Running Dynamics.Oikea Poikittainen Kiihtyvyys C1 Keskihajonta [m/s^2]","Running Dynamics.Vasen Poikittainen Kiihtyvyys C1 Keskihajonta [m/s^2]","Running Dynamics.Pystysuuntainen Kiihtyvyys C2 [m/s^2]","Running Dynamics.Pystysuuntainen Kiihtyvyys C2 Suodatettu [m/s^2]","Running Dynamics.Poikittainen Kiihtyvyys C2 [m/s^2]","Running Dynamics.Poikittainen Kiihtyvyys C2 Suodatettu [m/s^2]","Running Dynamics.Transversal Acceleration C2 Mean-to-Peak [m/s^2]","Running Dynamics.Pystysuuntainen Kiihtyvyys C3 [m/s^2]","Running Dynamics.Pystysuuntainen Kiihtyvyys C3 Suodatettu [m/s^2]","Running Dynamics.Poikittainen Kiihtyvyys C3 [m/s^2]","Running Dynamics.Poikittainen Kiihtyvyys C3 Suodatettu [m/s^2]","Running Dynamics.Transversal Acceleration C3 Mean-to-Peak [m/s^2]","Running Dynamics.Ajonopeus [Km/h]"\r\n' +
+  '318103,"008 KOKOL LR",630+0850.00,"64.07646857° N","24.54062901° E",55.985,-21.7708,26.3496,14.6794,14.1478,8.1315,8.0237,-4.1229,-6.3282,3.1816,-3.5781,1.3801,1.7761,2.2629,2.1137,-4.7717,-2.7778,-1.3045,1.3953,0.5937,1.2821,0.5037,0.3869,56\r\n' +
+  '318104,"008 KOKOL LR",630+0850.25,"64.07647082° N","24.54062896° E",55.955,29.2801,29.5273,16.7167,17.0519,8.1699,7.9743,3.8653,-6.4757,2.8971,-4.6735,1.3761,1.7859,2.1084,1.5889,2.7095,-2.0110,-1.0043,1.9055,0.3789,1.4189,0.6535,0.2594,56\r\n' +
+  '318105,"008 KOKOL LR",630+0850.50,"64.07647308° N","24.54062891° E",55.939,-29.2653,23.2646,-26.4762,-14.8906,8.4049,8.0362,4.1007,5.2723,-2.2401,4.5284,1.3860,1.7973,2.4118,2.1124,-2.5522,-1.9687,-1.3425,2.0056,-0.5608,1.6471,0.6996,0.2019,56\r\n' +
+  '318106,"008 KOKOL LR",630+0850.75,"64.07647533° N","24.54062885° E",55.938,-20.9017,-21.8595,13.2977,15.9086,8.4101,8.0505,4.0499,-4.9934,-2.2320,-3.1739,1.3767,1.7917,2.5858,2.0813,2.1109,0.8900,-1.1602,-1.8402,-0.5512,1.3017,0.6939,0.1295,56\r\n' +
+  '318107,"008 KOKOL LR",630+0851.00,"64.07647756° N","24.54062880° E",55.924,-21.2956,21.9569,14.8956,12.2093,8.3666,7.9174,-3.6308,4.0280,2.7983,2.0624,1.3882,1.7930,2.9541,2.8539,-2.6747,-1.8791,-1.2058,1.6617,-0.3302,1.8971,0.8156,0.1039,56\r\n' +
+  '318108,"008 KOKOL LR",630+0851.25,"64.07647979° N","24.54062875° E",55.925,-23.1085,25.8125,-21.9575,-12.1894,8.3251,7.8952,3.0163,-4.6372,2.5206,-2.8624,1.3800,1.8019,3.2062,3.0976,-3.2225,-1.4226,-2.0412,1.6744,-0.3927,2.0022,0.7377,0.0205,56';
+
 
 const amsCsvWithDifferentColumnOrder =
   'track,sscount,location,latitude,longitude,ajonopeus,oikea_pystysuuntainen_kiihtyvyys_c1,vasen_pystysuuntainen_kiihtyvyys_c1,oikea_pystysuuntainen_kiihtyvyys_c1_suodatettu,vasen_pystysuuntainen_kiihtyvyys_c1_suodatettu,oikea_pystysuuntainen_kiihtyvyys_c1_keskihajonta,vasen_pystysuuntainen_kiihtyvyys_c1_keskihajonta,oikea_poikittainen_kiihtyvyys_c1,vasen_poikittainen_kiihtyvyys_c1,oikea_poikittainen_kiihtyvyys_c1_suodatettu,vasen_poikittainen_kiihtyvyys_c1_suodatettu,oikea_poikittainen_kiihtyvyys_c1_keskihajonta,vasen_poikittainen_kiihtyvyys_c1_keskihajonta,pystysuuntainen_kiihtyvyys_c2,pystysuuntainen_kiihtyvyys_c2_suodatettu,poikittainen_kiihtyvyys_c2,poikittainen_kiihtyvyys_c2_suodatettu,transversal_acceleration_c2_mean_to_peak,pystysuuntainen_kiihtyvyys_c3,pystysuuntainen_kiihtyvyys_c3_suodatettu,poikittainen_kiihtyvyys_c3,poikittainen_kiihtyvyys_c3_suodatettu,transversal_acceleration_c3_mean_to_peak,ams_ajonopeus\r\n' +
@@ -283,6 +295,36 @@ describe('handle ams csv file success', () => {
   }, 900000);
 });
 
+//typo: "oikea_pystysuuntainen_kiityvyys_c1"
+describe('handle ams csv file unknow misspelled field success', () => {
+  test('success: normal run', async () => {
+    const dbConnection = undefined;
+    let missingColNames;
+    jest.spyOn(DBUtil.prototype,"writeMissingColumnsToDb").mockImplementation((reportId: number,
+                                                                               columnNames: string[],
+                                                                               dbConnection: DBConnection | undefined,)=>{missingColNames = columnNames; return new Promise((resolve, reject)=>resolve());});
+
+    const result = await parseCSVFileStream(
+      {
+        fileBaseName: 'chunkFile_889_1_AMS_20211125_003_YLORP_002_000_000.csv',
+        fileName: '',
+        fileSuffix: '',
+        keyWithoutSuffix:
+          '2022/Kamppis/20220202/20221024_TG_AMS_OHL_CW_Reports/252/LHRP/1/2022/Running Dynamics/20221024_133538/TextualReports/AMS_20221122_008_KOKOL_LR_630_630.csv',
+        rootFolder: '',
+        path: [],
+      },
+      stringToStream(amsCsvWithUnkownMisspelledField),
+      {},
+      dbConnection,
+    );
+    console.log(result);
+    console.log(missingColNames);
+    expect(result).toEqual('success');
+    expect(missingColNames).toEqual([ 'oikea_pystysuuntainen_kiihtyvyys_c1' ]);
+  }, 900000);
+});
+
 describe('handle ohlWithSurveyDate success', () => {
   test('success: normal run', async () => {
     const dbConnection = undefined;
@@ -308,6 +350,10 @@ describe('handle ohlWithSurveyDate success', () => {
 describe('handle rcWithSomeEnglishColNames success', () => {
   test('success: normal run', async () => {
     const dbConnection = undefined;
+    let missingColNames;
+    jest.spyOn(DBUtil.prototype,"writeMissingColumnsToDb").mockImplementation((reportId: number,
+                                                                               columnNames: string[],
+                                                                               dbConnection: DBConnection | undefined,)=>{missingColNames = columnNames; return new Promise((resolve, reject)=>resolve());});
     const result = await parseCSVFileStream(
       {
         fileBaseName: 'chunkFile_889_1_RC_20211125_003_YLORP_002_000_000.csv',
@@ -324,12 +370,19 @@ describe('handle rcWithSomeEnglishColNames success', () => {
     );
     console.log(result);
     expect(result).toEqual('success');
+    expect(missingColNames).toEqual(undefined);
   }, 900000);
 });
 
+
+//typo: "oikean_raiteen_aallon_rms_10_30mm_keskihajonta"
 describe('handle rcWithOikeaGenetiveForm success', () => {
   test('success: normal run', async () => {
     const dbConnection = undefined;
+    let missingColNames;
+    jest.spyOn(DBUtil.prototype,"writeMissingColumnsToDb").mockImplementation((reportId: number,
+                                                                               columnNames: string[],
+                                                                               dbConnection: DBConnection | undefined,)=>{missingColNames = columnNames; return new Promise((resolve, reject)=>resolve());});
     const result = await parseCSVFileStream(
       {
         fileBaseName: 'chunkFile_889_1_RC_20211125_003_YLORP_002_000_000.csv',
@@ -346,12 +399,19 @@ describe('handle rcWithOikeaGenetiveForm success', () => {
     );
     console.log(result);
     expect(result).toEqual('success');
+    expect(missingColNames).not.toContain("oikea_pystysuuntainen_kiityvyys_c1");
   }, 900000);
 });
 
+
+//typos: ["vasen_45kuluman_keskihajonta","vasen_ulkopuolisen_purseen_kiintea_keskihajonta"]
 describe('handle rpWithSomeTypo success', () => {
   test('success: normal run', async () => {
     const dbConnection = undefined;
+    let missingColNames;
+    jest.spyOn(DBUtil.prototype,"writeMissingColumnsToDb").mockImplementation((reportId: number,
+                                                                               columnNames: string[],
+                                                                               dbConnection: DBConnection | undefined,)=>{missingColNames = columnNames; return new Promise((resolve, reject)=>resolve());});
     const result = await parseCSVFileStream(
       {
         fileBaseName: 'chunkFile_889_1_RP_20211125_003_YLORP_002_000_000.csv',
@@ -368,12 +428,19 @@ describe('handle rpWithSomeTypo success', () => {
     );
     console.log(result);
     expect(result).toEqual('success');
+    expect(missingColNames).toEqual(undefined);
   }, 900000);
 });
 
+
+//typo: ["vasen_45kuluman_keskihajonta"]}
 describe('handle rpWithSomeTypo2 success', () => {
   test('success: normal run', async () => {
     const dbConnection = undefined;
+    let missingColNames;
+    jest.spyOn(DBUtil.prototype,"writeMissingColumnsToDb").mockImplementation((reportId: number,
+                                                                               columnNames: string[],
+                                                                               dbConnection: DBConnection | undefined,)=>{missingColNames = columnNames; return new Promise((resolve, reject)=>resolve());});
     const result = await parseCSVFileStream(
       {
         fileBaseName: 'chunkFile_889_1_RP_20211125_003_YLORP_002_000_000.csv',
@@ -390,12 +457,17 @@ describe('handle rpWithSomeTypo2 success', () => {
     );
     console.log(result);
     expect(result).toEqual('success');
+    expect(missingColNames).not.toContain("vasen_45kuluman_keskihajonta");
   }, 900000);
 });
 
 describe('handle tgSlave success', () => {
   test('success: normal run', async () => {
     const dbConnection = undefined;
+    let missingColNames;
+    jest.spyOn(DBUtil.prototype,"writeMissingColumnsToDb").mockImplementation((reportId: number,
+                                                                               columnNames: string[],
+                                                                               dbConnection: DBConnection | undefined,)=>{missingColNames = columnNames; return new Promise((resolve, reject)=>resolve());});
     const result = await parseCSVFileStream(
       {
         fileBaseName: 'chunkFile_889_1_TG_20211125_003_YLORP_002_000_000.csv',
@@ -412,6 +484,7 @@ describe('handle tgSlave success', () => {
     );
     console.log(result);
     expect(result).toEqual('success');
+    expect(missingColNames).toEqual(["vasen_korkeuspoikkema_d0", "oikea_korkeuspoikkema_d0", "vasen_korkeuspoikkema_d0_keskihajonta", "oikea_korkeuspoikkema_d0_keskihajonta"]);
   }, 900000);
 });
 
@@ -427,6 +500,10 @@ describe('handle ams csv file success', () => {
 describe('handle ams csv file with a missing field success', () => {
   test('success: normal run', async () => {
     const dbConnection = undefined;
+    let missingColNames;
+    jest.spyOn(DBUtil.prototype,"writeMissingColumnsToDb").mockImplementation((reportId: number,
+                                                                               columnNames: string[],
+                                                                               dbConnection: DBConnection | undefined,)=>{missingColNames = columnNames; return new Promise((resolve, reject)=>resolve());});
     let result = await parseCSVFileStream(
       {
         fileBaseName: 'chunkFile_889_1_AMS_20211125_003_YLORP_002_000_000.csv',
@@ -443,6 +520,7 @@ describe('handle ams csv file with a missing field success', () => {
     );
     console.log(result);
     expect(result).toEqual('success');
+    expect(missingColNames).toEqual(["sscount"]);
   }, 900000);
 });
 
