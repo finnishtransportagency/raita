93e1efae30d4ec6af1c3c06a38ef4ff0d6087cf6
Kalle Mustonen <kalle.mustonen@cgi.com>
2024-06-17 13:52:57 +0300

    move db using tests to integration folder and disable in jest

--- backend/lambdas/dataProcess/handleInspectionFileEvent/csvDataChopper/__tests__/integration/csvDataChopper.test.ts
+++ backend/lambdas/dataProcess/handleInspectionFileEvent/csvDataChopper/__tests__/integration/csvDataChopper.test.ts
@@ -1,10 +1,10 @@
-import { chopCSVFileStream } from '../csvDataChopper';
-import { stringToStream } from '../../__tests__/testUtils';
+import { chopCSVFileStream } from '../../csvDataChopper';
+import { stringToStream } from '../../../__tests__/testUtils';
 import * as fs from 'fs';
-import { log } from '../../../../../utils/logger';
-import { FileMetadataEntry } from '../../../../../types';
-import {DBUtil} from "../../../csvCommon/db/dbUtil";
-import {getKeyData} from "../../../../utils";
+import { log } from '../../../../../../utils/logger';
+import { FileMetadataEntry } from '../../../../../../types';
+import { DBUtil } from '../../../../csvCommon/db/dbUtil';
+
 
 const amsCsv =
   '"Running Date","22/11/2022 7:44:40 AM"\r\n' +
@@ -165,15 +165,77 @@ const ohlCsv: string =
   '25529,"006 KVRP 847",194+0415.00,"60.86959497° N","26.75753899° E",32.765,-55.91,,6204.75,,9.89,,0.00,0.20,5.31,,5.17,,0.34,,77.43,,0.45,,77.62,,0.0000,,,,33,,-20.48,2025.01';
 const ohlCsvStream = stringToStream(ohlCsv);
 
+const dbUtil = new DBUtil();
 
-describe('outfile name generation', () => {
+describe('insert raportti success', () => {
   test('success: normal run', async () => {
-    const key = getKeyData('Meeri/2021/Test/20210721/20210721_RP_Reports/005/ILMKON/1/2021/Rail+Profile/20210721_110925/TextualReports/RP_20210721_005_ILMKON_1_633_658.csv');
-    const pathString =  key.path.slice(0,key.path.length - 1).join('/');
-    const outFileName =
-      pathString + '/chunkFile_' + '123' + '_' + '1' + '_' + key.fileName;
+    const dbConnection = await dbUtil.getDBConnection();
+    const result = await dbUtil.insertRaporttiData(
+      'polku ja tiedostonimi',
+      'tiedostonimi',
+      'CHOPPING',
+      dbConnection
+    );
+    const updateData: Array<FileMetadataEntry> = [];
+    const row: FileMetadataEntry = {
+      errors: false,
+      bucket_arn: '',
+      bucket_name: '',
+      file_name: '',
+      hash: '',
+      key: '',
+      size: 0,
+      reportId: result,
+      metadata: {
+        source_system: "Meeri",
+        zip_reception__year: "2022",
+        campaign: "Kamppis",
+        zip_reception__date: "20220202",
+        zip_name: "20221020_TG_AMS_OHL_CW_Reports",
+        track_number: "554",
+        track_part: "KONVUS",
+        track_id: "XX",
+        year: 2022,
+        system: "OHL",
+        inspection_datetime: "2022-10-20T11:45:56.000Z",
+        report_category: "TextualReports",
+        file_type: "csv",
+        inspection_date: "2022-10-20T00:00:00.000Z",
+        km_start: 662,
+        km_end: 753,
+        parser_version: "1.2.0",
+        parsed_at_datetime: "2024-03-13T10:14:28.427Z"
+      },
+      options: {
+        skip_hash_check: true,
+      }
+    };
+    updateData.push(row);
+    await dbUtil.updateRaporttiMetadata(updateData, dbConnection);
+  });
+});
 
-    console.log('outFileName ' + outFileName);
-  })
+describe('handle ams file success', () => {
+  test('success: normal run', async () => {
+    const dbConnection = await dbUtil.getDBConnection();
+
+
+
+    const result = await chopCSVFileStream(
+      {
+        fileBaseName: 'AMS_20221122_008_KOKOL_LR_630_630.csv',
+        fileName: 'AMS_20221122_008_KOKOL_LR_630_630.csv',
+        fileSuffix: 'csv',
+        keyWithoutSuffix:
+          '2022/Kamppis/20220202/20221024_TG_AMS_OHL_CW_Reports/252/LHRP/1/2022/Running Dynamics/20221024_133538/TextualReports/VR_AMS_20221122_008_KOKOL_LR_630_630.csv',
+        rootFolder: '',
+        path: []
+      },
+      amsCsvStream,
+      dbConnection,
+      891,
+    );
+    log.debug('report inserted: ' + result);
+  }, 900000);
 });
 
