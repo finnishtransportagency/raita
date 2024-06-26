dfcccd0f3b6a6ea7c42bd3dc2708234fb13e1247
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2024-06-26 16:30:32 +0300

    fix tests

--- frontend/components/latest_inspection_date/__tests__/index.tsx
+++ frontend/components/latest_inspection_date/__tests__/index.tsx
@@ -2,13 +2,16 @@ import { render } from '@testing-library/react';
 
 import LatestInspectionDate from '../index';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MockedProvider } from '@apollo/client/testing';
 
 test('LatestInspectionDate', () => {
   const queryClient = new QueryClient();
 
   render(
     <QueryClientProvider client={queryClient}>
-      <LatestInspectionDate />
+      <MockedProvider>
+        <LatestInspectionDate />
+      </MockedProvider>
     </QueryClientProvider>,
   );
 });
