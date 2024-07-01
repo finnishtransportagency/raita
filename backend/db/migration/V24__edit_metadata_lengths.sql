62d9276317f387164ef5257f711d515b256d968d
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2024-07-01 16:06:16 +0300

    RAITA-558: increase length of varchar metadata types (#287)

---
+++ backend/db/migration/V24__edit_metadata_lengths.sql
@@ -0,0 +1,7 @@
+ALTER TABLE raportti ALTER COLUMN maintenance_area TYPE VARCHAR(50);
+ALTER TABLE raportti ALTER COLUMN tilirataosanumero TYPE VARCHAR(50);
+ALTER TABLE raportti ALTER COLUMN maintenance_area TYPE VARCHAR(50);
+ALTER TABLE raportti ALTER COLUMN measurement_start_location TYPE VARCHAR(50);
+ALTER TABLE raportti ALTER COLUMN measurement_end_location TYPE VARCHAR(50);
+ALTER TABLE raportti ALTER COLUMN measurement_direction TYPE VARCHAR(50);
+ALTER TABLE raportti ALTER COLUMN maintenance_level TYPE VARCHAR(50);
