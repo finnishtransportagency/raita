4cceb36a72786bdfc0e1e9fe664828058fbb9eef
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2024-07-08 14:08:28 +0300

    fix: always exit from ziphandler when done

--- backend/containers/zipHandler/src/processZipFile.ts
+++ backend/containers/zipHandler/src/processZipFile.ts
@@ -34,87 +34,91 @@ export const processZipFile = ({
         .catch(err => reject(err));
     };
 
-    yauzl.open(filePath, { lazyEntries: true }, (err, zipfile) => {
-      // Prosessing of each entry adds a promise into the entryPromises array
-      const entries: Array<Promise<EntryRecord>> = [];
-      // If opening zip files fails, reject promise and return;
-      if (err) {
-        reject(err);
-        return;
-      }
-      // Handler to run when zipfile is processed
-      // Resolves the main zip file promise with payload of
-      // file results all entries by resolving entry promises
-      zipfile.on('end', () => {
-        closeProcess(entries);
-      });
-      // Handler to run for eacn entry in the file
-      zipfile.on('entry', entry => {
-        if (/\/$/.test(entry.fileName)) {
-          // Entry is directory, can be ignored
-          zipfile.readEntry();
-        } else {
-          // Entry is a file
-          // Inspect the enty file name to detect video files: video files are not extracted from the zip
-          if (isRaitaVideoFile(entry.fileName)) {
-            // Ignore the current video file entry and read next
-            entries.push(
-              Promise.resolve({
-                fileName: entry.fileName,
-                status: 'skipped',
-                errorDescription: '',
-                compressedSize: NaN, // sizes are not needed
-                uncompressedSize: NaN,
-              }),
-            );
+    yauzl.open(
+      filePath,
+      { lazyEntries: true, autoClose: true },
+      (err, zipfile) => {
+        // Prosessing of each entry adds a promise into the entryPromises array
+        const entries: Array<Promise<EntryRecord>> = [];
+        // If opening zip files fails, reject promise and return;
+        if (err) {
+          reject(err);
+          return;
+        }
+        // Handler to run when zipfile is processed
+        // Resolves the main zip file promise with payload of
+        // file results all entries by resolving entry promises
+        zipfile.on('end', () => {
+          closeProcess(entries);
+        });
+        // Handler to run for eacn entry in the file
+        zipfile.on('entry', entry => {
+          if (/\/$/.test(entry.fileName)) {
+            // Entry is directory, can be ignored
             zipfile.readEntry();
           } else {
-            // Generate key by joining the prefix to the path of the file inside zip
-            // Key determines the path where file is stored in target bucket
-            const key = `${s3KeyPrefix}/${entry.fileName.toString()}`;
-            zipfile.openReadStream(entry, (err, readStream) => {
-              // Returns a promise for entry which resolves when file is uploaded to S3 (or upload fails)
-              const entryResult = new Promise<EntryRecord>(resolveEntry => {
-                if (err) {
-                  closeProcess(entries, err);
-                }
-                readStream.on('error', err => {
-                  closeProcess(entries, err);
-                });
-                readStream.once('end', () => {
-                  zipfile.readEntry();
-                });
-                // Get write stream and a promise which resolves as streaming the data to S3 is complete
-                const { writeStream, uploadPromise } = uploadToS3({
-                  targetBucket,
-                  key,
-                  s3,
-                  zipFileData,
-                });
-                readStream.pipe(writeStream);
-                uploadPromise
-                  .then(() => {
-                    // Upload succeeded, resolve with success
-                    resolveEntry({ ...entry, status: 'success' });
-                  })
-                  .catch((err: unknown) => {
-                    // Upload failed, resolve with error
-                    resolveEntry({
-                      ...entry,
-                      fileName: entry.fileName.toString(),
-                      status: 'error',
-                      errorDescription: `Upload to S3 failed: ${
-                        err instanceof Error ? err.message : err
-                      }`,
-                    });
+            // Entry is a file
+            // Inspect the enty file name to detect video files: video files are not extracted from the zip
+            if (isRaitaVideoFile(entry.fileName)) {
+              // Ignore the current video file entry and read next
+              entries.push(
+                Promise.resolve({
+                  fileName: entry.fileName,
+                  status: 'skipped',
+                  errorDescription: '',
+                  compressedSize: NaN, // sizes are not needed
+                  uncompressedSize: NaN,
+                }),
+              );
+              zipfile.readEntry();
+            } else {
+              // Generate key by joining the prefix to the path of the file inside zip
+              // Key determines the path where file is stored in target bucket
+              const key = `${s3KeyPrefix}/${entry.fileName.toString()}`;
+              zipfile.openReadStream(entry, (err, readStream) => {
+                // Returns a promise for entry which resolves when file is uploaded to S3 (or upload fails)
+                const entryResult = new Promise<EntryRecord>(resolveEntry => {
+                  if (err) {
+                    closeProcess(entries, err);
+                  }
+                  readStream.on('error', err => {
+                    closeProcess(entries, err);
                   });
+                  readStream.once('end', () => {
+                    zipfile.readEntry();
+                  });
+                  // Get write stream and a promise which resolves as streaming the data to S3 is complete
+                  const { writeStream, uploadPromise } = uploadToS3({
+                    targetBucket,
+                    key,
+                    s3,
+                    zipFileData,
+                  });
+                  readStream.pipe(writeStream);
+                  uploadPromise
+                    .then(() => {
+                      // Upload succeeded, resolve with success
+                      resolveEntry({ ...entry, status: 'success' });
+                    })
+                    .catch((err: unknown) => {
+                      // Upload failed, resolve with error
+                      resolveEntry({
+                        ...entry,
+                        fileName: entry.fileName.toString(),
+                        status: 'error',
+                        errorDescription: `Upload to S3 failed: ${
+                          err instanceof Error ? err.message : err
+                        }`,
+                      });
+                    });
+                });
+                // Add promise to array of entries
+                entries.push(entryResult);
               });
-              // Add promise to array of entries
-              entries.push(entryResult);
-            });
+            }
           }
-        }
-      });
-      zipfile.readEntry();
-    });
+        });
+        zipfile.readEntry();
+      },
+    );
   });
