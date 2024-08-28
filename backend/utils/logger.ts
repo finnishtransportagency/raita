ef835c4abb2d6c2ddbb142a43fce7669b8e73546
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2024-08-28 14:49:39 +0300

    feat: don't filter db query logs as any error

--- backend/utils/logger.ts
+++ backend/utils/logger.ts
@@ -31,6 +31,7 @@ function getLogger(tag: string) {
 }
 
 export const log = getLogger('RAITA_BACKEND');
+export const logDatabaseOperation = getLogger('RAITA_DATABASE_OPERATION');
 export const logParsingException = getLogger('RAITA_PARSING_EXCEPTION');
 export const logCSVParsingException = getLogger('RAITA_CSV_PARSING_EXCEPTION');
 export const logCSVDBException = getLogger('RAITA_CSV_DB_EXCEPTION');
