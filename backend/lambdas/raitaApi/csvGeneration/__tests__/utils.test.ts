fad345adbccb816f5584c7f20775408223c493ed
KPuranen <71770030+KPuranen@users.noreply.github.com>
2024-10-23 10:36:40 +0300

    RAITA-622/csv-to-excel-usable-form (#362)
    * RAITA-622/csv-to-excel-usable-form
    
    * test fixes

--- backend/lambdas/raitaApi/csvGeneration/__tests__/utils.test.ts
+++ backend/lambdas/raitaApi/csvGeneration/__tests__/utils.test.ts
@@ -5,13 +5,13 @@ const testData = [
   { test1: 'z', date: '2', variable1: '3', test2: '4' },
   { test1: 'z', date: 'b', variable1: 'a', test2: 'c' },
 ];
-const headerString = 'test1,date,variable1,test2\r\n';
-const bodyString = 'z,foo,bar,baz\r\n' + 'z,2,3,4\r\n' + 'z,b,a,c\r\n';
+const headerString = 'test1;date;variable1;test2\r\n';
+const bodyString = 'z;foo;bar;baz\r\n' + 'z;2;3;4\r\n' + 'z;b;a;c\r\n';
 const csvString =
-  'test1,date,variable1,test2\r\n' +
-  'z,foo,bar,baz\r\n' +
-  'z,2,3,4\r\n' +
-  'z,b,a,c\r\n';
+  'test1;date;variable1;test2\r\n' +
+  'z;foo;bar;baz\r\n' +
+  'z;2;3;4\r\n' +
+  'z;b;a;c\r\n';
 describe('objectToCsvHeader', () => {
   test('success: basic operation', () => {
     const header = objectToCsvHeader(testData[0]);
