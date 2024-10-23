d026dd30101c2b87cefae46e4ba6ae08f696c259
Konsta Puranen <konstapuranen@gmail.com>
2024-10-23 09:37:24 +0300

    test fixes

--- backend/lambdas/raitaApi/csvGeneration/__tests__/utils.test.ts
+++ backend/lambdas/raitaApi/csvGeneration/__tests__/utils.test.ts
@@ -5,23 +5,23 @@ const testData = [
   { test1: 'z', date: '2', variable1: '3', test2: '4' },
   { test1: 'z', date: 'b', variable1: 'a', test2: 'c' },
 ];
-const headerString = 'test1,date,variable1,test2\r\n';
-const bodyString = 'z,foo,bar,baz\r\n' + 'z,2,3,4\r\n' + 'z,b,a,c\r\n';
+const headerString = 'test1;date;variable1;test2\r\n';
+const bodyString = 'z;foo;bar;baz\r\n' + 'z;2;3;4\r\n' + 'z;b;a;c\r\n';
 const csvString =
-  'test1,date,variable1,test2\r\n' +
-  'z,foo,bar,baz\r\n' +
-  'z,2,3,4\r\n' +
-  'z,b,a,c\r\n';
+  'test1;date;variable1;test2\r\n' +
+  'z;foo;bar;baz\r\n' +
+  'z;2;3;4\r\n' +
+  'z;b;a;c\r\n';
 describe('objectToCsvHeader', () => {
   test('success: basic operation', () => {
     const header = objectToCsvHeader(testData[0]);
-    expect(header).toEqual(headerString.replace(/,/g, ';'));
+    expect(header).toEqual(headerString);
   });
 });
 describe('objectToCsvBody', () => {
   test('success: basic operation', () => {
     const body = objectToCsvBody(testData);
-    expect(body).toEqual(bodyString.replace(/,/g, ';'));
+    expect(body).toEqual(bodyString);
   });
 });
 describe('objectToCsvHeader and objectToCsvBody', () => {
@@ -29,6 +29,6 @@ describe('objectToCsvHeader and objectToCsvBody', () => {
     // important to make sure header ans body columns are aligned
     const header = objectToCsvHeader(testData[0]);
     const body = objectToCsvBody(testData);
-    expect(header + body).toEqual(csvString.replace(/,/g, ';'));
+    expect(header + body).toEqual(csvString);
   });
 });
