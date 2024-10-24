0eb0ab6b0358412f6bf17efe73a72e21d7bed1c9
KPuranen <71770030+KPuranen@users.noreply.github.com>
2024-10-24 12:46:30 +0300

    fix/RAITA-668-graphql-uri-fix (#366)

--- frontend/shared/graphql/client.ts
+++ frontend/shared/graphql/client.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { baseURL } from 'shared/config';
 
 export const apolloClient = new ApolloClient({
-  uri: '/api/v2/graphql',
+  uri: `${baseURL}/v2/graphql`,
   cache: new InMemoryCache(),
 });
