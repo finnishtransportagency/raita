d9fe9e7a9ac7be099fd9d399186755b8160c45a8
Konsta Puranen <konstapuranen@gmail.com>
2024-11-05 14:08:56 +0200

    pipelinelock to use prisma

--- backend/utils/dataProcessLock.ts
+++ backend/utils/dataProcessLock.ts
@@ -1,5 +1,6 @@
 import { subMinutes } from 'date-fns';
-import { PostgresDBConnection } from '../lambdas/dataProcess/csvCommon/db/dbUtil';
+import { DBConnection } from '../lambdas/dataProcess/csvCommon/db/dbUtil';
+import { PrismaClient } from '@prisma/client';
 
 /**
  * This file handles the data process lock
@@ -29,33 +30,39 @@ async function acquireLockOrFail(
   holderType: LockHolderType,
   holderTypeToFailOn: LockHolderType,
   filename: string | null,
-  dbConnection: PostgresDBConnection,
+  dbConnection: DBConnection,
 ) {
-  const sql = dbConnection.sql;
-  const schema = dbConnection.schema;
+  const { prisma } = dbConnection;
   const timestamp = new Date(Date.now()).toISOString();
   const expiredTime = subMinutes(
     new Date(),
     LOCK_TIMEOUT_MINUTES,
   ).toISOString();
   try {
-    const selectExistingPipelineLock = sql`
-    SELECT FROM ${sql(
-      schema,
-    )}.data_process_lock WHERE holder_type = ${holderTypeToFailOn} AND create_time > ${expiredTime}`;
-    const result = await sql.begin(async innerSql => {
-      await innerSql`
-      LOCK TABLE ${innerSql(schema)}.data_process_lock;
-        `;
-      return await innerSql`
-      INSERT INTO ${innerSql(schema)}.data_process_lock
-      (holder_type, zip_file_name, create_time)
-      SELECT ${holderType}, ${filename}, ${timestamp}
-      WHERE NOT EXISTS (
-        ${selectExistingPipelineLock}
-      )
-      RETURNING *;
-      `;
+    const result = await prisma.$transaction(async (prisma: PrismaClient) => {
+      // Check if an existing lock already meets the criteria
+      const existingPipelineLock = await prisma.data_process_lock.findFirst({
+        where: {
+          holder_type: holderTypeToFailOn,
+          create_time: {
+            gt: expiredTime,
+          },
+        },
+      });
+
+      // If no existing lock meets the criteria, create a new one
+      if (!existingPipelineLock) {
+        return await prisma.data_process_lock.create({
+          data: {
+            holder_type: holderType,
+            zip_file_name: filename,
+            create_time: timestamp,
+          },
+        });
+      }
+
+      // Return null or handle the case when the lock already exists
+      return null;
     });
     if (result.length === 0) {
       throw new DataProcessLockedError();
@@ -71,18 +78,23 @@ async function acquireLockOrFail(
 async function releaseLock(
   holderType: LockHolderType,
   filename: string | null,
-  dbConnection: PostgresDBConnection,
+  dbConnection: DBConnection,
 ) {
-  const sql = dbConnection.sql;
-  const schema = dbConnection.schema;
-  return await sql`
-  DELETE FROM ${sql(schema)}.data_process_lock
-  WHERE holder_type = ${holderType}
-  AND zip_file_name = ${filename};
-  `;
+  const { prisma } = dbConnection;
+  try {
+    const result = await prisma.data_process_lock.deleteMany({
+      where: {
+        holder_type: holderType,
+        zip_file_name: filename,
+      },
+    });
+    return result;
+  } catch (error) {
+    throw new DataProcessLockedError();
+  }
 }
 
-export function acquirePipelineLockOrFail(dbConnection: PostgresDBConnection) {
+export function acquirePipelineLockOrFail(dbConnection: DBConnection) {
   return acquireLockOrFail(
     LockHolderType.Pipeline,
     LockHolderType.DataProcess,
@@ -92,7 +104,7 @@ export function acquirePipelineLockOrFail(dbConnection: PostgresDBConnection) {
 }
 export function acquireDataProcessLockOrFail(
   filename: string,
-  dbConnection: PostgresDBConnection,
+  dbConnection: DBConnection,
 ) {
   return acquireLockOrFail(
     LockHolderType.DataProcess,
@@ -101,12 +113,12 @@ export function acquireDataProcessLockOrFail(
     dbConnection,
   );
 }
-export function releasePipelineLock(dbConnection: PostgresDBConnection) {
+export function releasePipelineLock(dbConnection: DBConnection) {
   return releaseLock(LockHolderType.Pipeline, 'pipeline', dbConnection);
 }
 export function releaseDataProcessLock(
   filename: string,
-  dbConnection: PostgresDBConnection,
+  dbConnection: DBConnection,
 ) {
   return releaseLock(LockHolderType.DataProcess, filename, dbConnection);
 }
@@ -114,13 +126,14 @@ export function releaseDataProcessLock(
 /**
  * This should only be used from the pipeline
  */
-export async function lockTableExists(dbConnection: PostgresDBConnection) {
-  const sql = dbConnection.sql;
-  const schema = dbConnection.schema;
-  const result = await sql`SELECT EXISTS (
-  SELECT FROM information_schema.tables
-  WHERE table_schema = ${schema}
-  AND table_name = 'data_process_lock'
-  );`;
+export async function lockTableExists(dbConnection: DBConnection) {
+  const { prisma } = dbConnection;
+  const result = await prisma.$queryRaw`
+  SELECT EXISTS (
+    SELECT 1
+    FROM information_schema.tables
+    WHERE table_name = 'data_process_lock'
+  ) as "exists";
+`;
   return Boolean(result[0].exists);
 }
