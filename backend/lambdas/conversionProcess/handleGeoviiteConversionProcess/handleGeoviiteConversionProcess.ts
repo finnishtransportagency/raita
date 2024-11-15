35d97f32d679a2534fb6d8f60f23c7ab86f5bdaf
Kalle Mustonen <kalle.mustonen@cgi.com>
2024-11-15 14:07:32 +0200

    Test small batch.

--- backend/lambdas/conversionProcess/handleGeoviiteConversionProcess/handleGeoviiteConversionProcess.ts
+++ backend/lambdas/conversionProcess/handleGeoviiteConversionProcess/handleGeoviiteConversionProcess.ts
@@ -125,7 +125,7 @@ export async function handleGeoviiteConversionProcess(
       }
 
       // save result all at once to save db call time cost
-      const saveBatchSize = 1000;
+      const saveBatchSize = 10;
 
       // one timestamp for all
       const timestamp = new Date();
