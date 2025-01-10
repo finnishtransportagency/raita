a174e99c0717a583029598ed9d79882713c52dc5
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2025-01-10 19:19:40 +0200

    fix: ignore errors caused by translation mismatch in loading page

--- frontend/components/loading-overlay.tsx
+++ frontend/components/loading-overlay.tsx
@@ -7,10 +7,13 @@ export function LoadingOverlay() {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
       <Spinner size={12} />
-      <h2 className="text-center text-white text-xl font-semibold">
+      <h2
+        className="text-center text-white text-xl font-semibold"
+        suppressHydrationWarning
+      >
         {t('common:loading')}
       </h2>
-      <p className="w-1/3 text-center text-white">
+      <p className="w-1/3 text-center text-white" suppressHydrationWarning>
         {t('common:iam_problem_contact_info')}
       </p>
       {/* Hardcoded error message if javascript isn't working. */}
