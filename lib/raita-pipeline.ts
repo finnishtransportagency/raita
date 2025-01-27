7dbdc71d4d25bacc6743b7a724f6c15445285b2a
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2025-01-27 14:10:21 +0200

    feat: use correct docker credentials in build process, change credential secret format

--- lib/raita-pipeline.ts
+++ lib/raita-pipeline.ts
@@ -12,6 +12,7 @@ import {
   CodeBuildStep,
   CodePipeline,
   CodePipelineSource,
+  DockerCredential,
   ShellStep,
   Step,
 } from 'aws-cdk-lib/pipelines';
@@ -42,6 +43,7 @@ import { RaitaPipelineLockStack } from './raita-pipeline-lock';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Code, LayerVersion, Runtime } from 'aws-cdk-lib/aws-lambda';
 import path from 'path';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 
 /**
  * The stack that defines the application pipeline
@@ -149,11 +151,26 @@ export class RaitaPipelineStack extends Stack {
         })
       : null;
 
+    // secret contains credentials as json
+    // { "username": "x", "secret": "x"}
+    const dockerCredentialsSecret = Secret.fromSecretNameV2(
+      this,
+      'docker-credentials',
+      'docker-hub-credentials',
+    );
+    const dockerCredentials = DockerCredential.dockerHub(
+      dockerCredentialsSecret,
+      {
+        secretPasswordField: 'secret',
+        secretUsernameField: 'username',
+      },
+    );
     const codePipeline = new CodePipeline(
       this,
       `pipeline-raita-${config.stackId}`,
       {
         codePipeline: pipeline,
+        dockerCredentials: [dockerCredentials],
         synth: new ShellStep('Synth', {
           input: githubSource,
           installCommands: [
@@ -219,7 +236,11 @@ export class RaitaPipelineStack extends Stack {
             },
             DOCKER_PASSWORD: {
               type: BuildEnvironmentVariableType.SECRETS_MANAGER,
-              value: 'docker_password',
+              value: 'docker-hub-credentials:secret',
+            },
+            DOCKER_USERNAME: {
+              type: BuildEnvironmentVariableType.SECRETS_MANAGER,
+              value: 'docker-hub-credentials:username',
             },
             CONF_FILE_DIR: {
               type: BuildEnvironmentVariableType.PLAINTEXT,
@@ -231,7 +252,7 @@ export class RaitaPipelineStack extends Stack {
           'echo $CONF_FILE_DIR',
           'echo Logging in to Docker hub...',
           dbConfEditCommand,
-          'docker login -u=raita2dockeruser -p=$DOCKER_PASSWORD',
+          'docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD',
           'docker run --rm -v $(pwd)/backend/db/migration:/flyway/sql -v $(pwd)/backend/db/conf/$CONF_FILE_DIR:/flyway/conf flyway/flyway migrate -password=$DB_PASSWORD',
         ],
       }),
