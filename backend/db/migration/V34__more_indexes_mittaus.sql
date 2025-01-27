688fc1bb63e55ab06179ec37d5b4de2dd0fdc739
Kalle Mustonen <kalle.mustonen@cgi.com>
2025-01-27 11:32:50 +0200

    updated_at indexit olikin jo olemassa, poistin tästä

--- backend/db/migration/V34__more_indexes_mittaus.sql
+++ backend/db/migration/V34__more_indexes_mittaus.sql
@@ -7,15 +7,6 @@ create index concurrently if not exists rp_mittaus_multi_index on rp_mittaus(rap
 create index concurrently if not exists tg_mittaus_multi_index on tg_mittaus(raportti_id, id);
 create index concurrently if not exists tsight_mittaus_multi_index on tsight_mittaus(raportti_id, id);
 
-create index concurrently if not exists geoviite_updated_at_index on mittaus(geoviite_updated_at);
-create index concurrently if not exists ams_geoviite_updated_at_index on ams_mittaus(geoviite_updated_at);
-create index concurrently if not exists ohl_geoviite_updated_at_index on ohl_mittaus(geoviite_updated_at);
-create index concurrently if not exists pi_geoviite_updated_at_index on pi_mittaus(geoviite_updated_at);
-create index concurrently if not exists rc_geoviite_updated_at_index on rc_mittaus(geoviite_updated_at);
-create index concurrently if not exists rp_geoviite_updated_at_index on rp_mittaus(geoviite_updated_at);
-create index concurrently if not exists tg_geoviite_updated_at_index on tg_mittaus(geoviite_updated_at);
-create index concurrently if not exists tsight_geoviite_updated_at_index on tsight_mittaus(geoviite_updated_at);
-
 create index concurrently if not exists geoviite_virhe_index on mittaus(geoviite_virhe);
 create index concurrently if not exists ams_geoviite_virhe_index on ams_mittaus(geoviite_virhe);
 create index concurrently if not exists ohl_geoviite_virhe_index on ohl_mittaus(geoviite_virhe);
