644b80f08653d58f66fd9e6967ccd9ddd051a2e1
Kalle Mustonen <kalle.mustonen@cgi.com>
2025-02-04 09:52:24 +0200

    multi index with ids other way round

---
+++ backend/db/migration/V35__more_indexes_mittaus_part_twp.sql
@@ -0,0 +1,8 @@
+create index concurrently if not exists mittaus_multi_index2 on mittaus(id,raportti_id);
+create index concurrently if not exists ams_mittaus_multi_index2 on ams_mittaus(id,raportti_id);
+create index concurrently if not exists ohl_mittaus_multi_index2 on ohl_mittaus(id,raportti_id);
+create index concurrently if not exists pi_mittaus_multi_index2 on pi_mittaus(id,raportti_id);
+create index concurrently if not exists rc_mittaus_multi_index2 on rc_mittaus(id,raportti_id);
+create index concurrently if not exists rp_mittaus_multi_index2 on rp_mittaus(id,raportti_id);
+create index concurrently if not exists tg_mittaus_multi_index2 on tg_mittaus(id,raportti_id);
+create index concurrently if not exists tsight_mittaus_multi_index2 on tsight_mittaus(id,raportti_id);
