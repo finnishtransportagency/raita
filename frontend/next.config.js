9a989cc61bde744ff09975acc36ff516570a0213
Tarmo Asikainen <tarmo.asikainen@cgi.com>
2025-02-18 17:55:54 +0200

    version upgrades

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,31 +1,42 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: false,
-  compiler: {
-    removeConsole: false,
-  },
-  basePath: process.env.NEXT_PUBLIC_RAITA_BASEURL || '',
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.md/,
-      type: 'asset/source',
-    });
-    return config;
-  },
-  images: {
-    unoptimized: true,
-  },
-  rewrites: () => {
-    return {
-      beforeFiles: [
-        {
-          source: '/',
-          destination: '/reports',
-        },
-      ],
-    };
-  },
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+const getNextConfig = (phase, { defaultConfig }) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    output: isDev ? 'standalone' : 'export',
+    reactStrictMode: true,
+    swcMinify: false,
+    compiler: {
+      removeConsole: false,
+    },
+    basePath: process.env.NEXT_PUBLIC_RAITA_BASEURL || '',
+    webpack: config => {
+      config.module.rules.push({
+        test: /\.md/,
+        type: 'asset/source',
+      });
+      return config;
+    },
+    images: {
+      unoptimized: true,
+    },
+    rewrites: isDev
+      ? () => {
+          return {
+            beforeFiles: [
+              {
+                source: '/',
+                destination: '/reports',
+              },
+            ],
+          };
+        }
+      : {},
+  };
+  return nextConfig;
 };
 
-module.exports = nextConfig;
+module.exports = getNextConfig;
